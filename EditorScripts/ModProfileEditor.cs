b22ecea77a46fde9d77a83607bf922bebb4972c5
Jackson Wood <jackson@melodatron.com>
2018-04-10 17:08:31 +1000

    ScriptableModProfile added

---
+++ EditorScripts/ModProfileEditor.cs
@@ -0,0 +1,31 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+namespace ModIO
+{
+    [CustomEditor(typeof(ScriptableModProfile))]
+    public class ModProfileEditor : Editor
+    {
+        // ---------[ EDITOR CACHING ]---------
+        private ModProfile profile;
+
+        private void OnEnable()
+        {
+
+        }
+
+        private void OnDisable()
+        {
+
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.LabelField("DISPLAYING MOD PROFILE");
+        }
+    }
+}
+
+#endif
---
+++ Scripts/ModCustomization/ScriptableModProfile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace ModIO
+{
+    [CreateAssetMenu(fileName = "New Mod Profile", menuName = "ModIO/Create Mod Profile")]
+    public class ScriptableModProfile : ScriptableObject
+    {
+        public int modId = 0;
+        public EditableModProfile modEdits = new EditableModProfile();
+    }
+}
