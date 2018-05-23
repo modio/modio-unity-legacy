f072bbc99fc78a76a70d94b7920f3355298394b2
Jackson Wood <jackson@melodatron.com>
2018-05-23 18:25:30 +1000

    Mod Submission Tool is now disabled while uploading

--- EditorScripts/ModSubmissionToolWindow.cs
+++ EditorScripts/ModSubmissionToolWindow.cs
@@ -76,143 +76,146 @@ namespace ModIO
         // ------[ LOGIN PROMPT ]------
         protected virtual void LayoutSubmissionFields()
         {
-            // - Account Header -
-            EditorGUILayout.BeginHorizontal();
+            using(new EditorGUI.DisabledScope(isAwaitingServerResponse))
             {
-                if(this.user == null)
+                // - Account Header -
+                EditorGUILayout.BeginHorizontal();
                 {
-                    EditorGUILayout.LabelField("Not logged in to mod.io");
-                    GUILayout.FlexibleSpace();
-                    if(GUILayout.Button("Log In"))
+                    if(this.user == null)
                     {
-                        LoginWindow.GetWindow<LoginWindow>("Login to mod.io");
+                        EditorGUILayout.LabelField("Not logged in to mod.io");
+                        GUILayout.FlexibleSpace();
+                        if(GUILayout.Button("Log In"))
+                        {
+                            LoginWindow.GetWindow<LoginWindow>("Login to mod.io");
+                        }
                     }
+                    else
+                    {
+                        EditorGUILayout.LabelField("Logged in as:  " + this.user.username);
+                        GUILayout.FlexibleSpace();
+                        if(GUILayout.Button("Log Out"))
+                        {
+                            EditorApplication.delayCall += () =>
+                            {
+                                if(EditorDialogs.ConfirmLogOut(this.user.username))
+                                {
+                                    APIClient.userAuthorizationToken = null;
+                                    CacheClient.DeleteAuthenticatedUser();
+
+                                    isAwaitingServerResponse = false;
+
+                                    Repaint();
+                                }
+                            };
+                        }
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+                // - Submission Section -
+                if(!String.IsNullOrEmpty(uploadSucceededMessage))
+                {
+                    EditorGUILayout.HelpBox(uploadSucceededMessage,
+                                            MessageType.Info);
+                }
+                else if(!String.IsNullOrEmpty(uploadFailedMessage))
+                {
+                    EditorGUILayout.HelpBox(uploadFailedMessage,
+                                            MessageType.Error);
+                }
+                else if(profile == null)
+                {
+                    EditorGUILayout.HelpBox("Please select a mod profile as a the upload target.",
+                                            MessageType.Info);
+                }
+                else if(profile.modId > 0)
+                {
+                    EditorGUILayout.HelpBox(profile.editableModProfile.name.value
+                                            + " will be updated as used as the upload target on the server.",
+                                            MessageType.Info);
                 }
                 else
                 {
-                    EditorGUILayout.LabelField("Logged in as:  " + this.user.username);
-                    GUILayout.FlexibleSpace();
-                    if(GUILayout.Button("Log Out"))
-                    {
-                        EditorApplication.delayCall += () =>
-                        {
-                            if(EditorDialogs.ConfirmLogOut(this.user.username))
-                            {
-                                APIClient.userAuthorizationToken = null;
-                                CacheClient.DeleteAuthenticatedUser();
-
-                                isAwaitingServerResponse = false;
-
-                                Repaint();
-                            }
-                        };
-                    }
+                    EditorGUILayout.HelpBox(profile.editableModProfile.name.value
+                                            + " will be created as a new profile on the server.",
+                                            MessageType.Info);
                 }
-            }
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-
-            // - Submission Section -
-            if(!String.IsNullOrEmpty(uploadSucceededMessage))
-            {
-                EditorGUILayout.HelpBox(uploadSucceededMessage,
-                                        MessageType.Info);
-            }
-            else if(!String.IsNullOrEmpty(uploadFailedMessage))
-            {
-                EditorGUILayout.HelpBox(uploadFailedMessage,
-                                        MessageType.Error);
-            }
-            else if(profile == null)
-            {
-                EditorGUILayout.HelpBox("Please select a mod profile as a the upload target.",
-                                        MessageType.Info);
-            }
-            else if(profile.modId > 0)
-            {
-                EditorGUILayout.HelpBox(profile.editableModProfile.name.value
-                                        + " will be updated as used as the upload target on the server.",
-                                        MessageType.Info);
-            }
-            else
-            {
-                EditorGUILayout.HelpBox(profile.editableModProfile.name.value
-                                        + " will be created as a new profile on the server.",
-                                        MessageType.Info);
-            }
-            EditorGUILayout.Space();
+                EditorGUILayout.Space();
 
 
-            // TODO(@jackson): Support mods that haven't been downloaded
-            profile = EditorGUILayout.ObjectField("Mod Profile",
-                                                  profile,
-                                                  typeof(ScriptableModProfile),
-                                                  false) as ScriptableModProfile;
-
-            // - Build Profile -
-            using(new EditorGUI.DisabledScope(profile == null))
-            {
-                EditorGUILayout.BeginHorizontal();
-                    if(EditorGUILayoutExtensions.BrowseButton(buildFilePath, new GUIContent("Modfile")))
-                    {
-                        EditorApplication.delayCall += () =>
-                        {
-                            string path = EditorUtility.OpenFilePanelWithFilters("Set Build Location",
-                                                                                 "",
-                                                                                 modBinaryFileExtensionFilters);
-
-                            if (path.Length != 0)
-                            {
-                                buildFilePath = path;
-                            }
-                        };
-                    }
-                    if(EditorGUILayoutExtensions.ClearButton())
-                    {
-                        buildFilePath = string.Empty;
-                    }
-                EditorGUILayout.EndHorizontal();
+                // TODO(@jackson): Support mods that haven't been downloaded
+                profile = EditorGUILayout.ObjectField("Mod Profile",
+                                                      profile,
+                                                      typeof(ScriptableModProfile),
+                                                      false) as ScriptableModProfile;
 
                 // - Build Profile -
-                using(new EditorGUI.DisabledScope(!System.IO.File.Exists(buildFilePath)))
+                using(new EditorGUI.DisabledScope(profile == null))
                 {
-                    // - Version -
-                    EditorGUI.BeginChangeCheck();
-                        buildProfile.version.value = EditorGUILayout.TextField("Version",
-                                                                               buildProfile.version.value);
-                    if(EditorGUI.EndChangeCheck())
-                    {
-                        buildProfile.version.isDirty = true;
-                    }
-                    // - Changelog -
-                    EditorGUI.BeginChangeCheck();
-                        EditorGUILayout.PrefixLabel("Changelog");
-                        buildProfile.changelog.value = EditorGUILayoutExtensions.MultilineTextField(buildProfile.changelog.value);
-                    if(EditorGUI.EndChangeCheck())
-                    {
-                        buildProfile.changelog.isDirty = true;
-                    }
-                    // - Metadata -
-                    EditorGUI.BeginChangeCheck();
-                        EditorGUILayout.PrefixLabel("Metadata");
-                        buildProfile.metadataBlob.value = EditorGUILayoutExtensions.MultilineTextField(buildProfile.metadataBlob.value);
-                    if(EditorGUI.EndChangeCheck())
-                    {
-                        buildProfile.metadataBlob.isDirty = true;
-                    }
-                }
+                    EditorGUILayout.BeginHorizontal();
+                        if(EditorGUILayoutExtensions.BrowseButton(buildFilePath, new GUIContent("Modfile")))
+                        {
+                            EditorApplication.delayCall += () =>
+                            {
+                                string path = EditorUtility.OpenFilePanelWithFilters("Set Build Location",
+                                                                                     "",
+                                                                                     modBinaryFileExtensionFilters);
 
-                // TODO(@jackson): if(profile) -> show build list?
-                EditorGUILayout.Space();
-                EditorGUILayout.BeginHorizontal();
-                    GUILayout.FlexibleSpace();
-                    if(GUILayout.Button("Upload to Server"))
+                                if (path.Length != 0)
+                                {
+                                    buildFilePath = path;
+                                }
+                            };
+                        }
+                        if(EditorGUILayoutExtensions.ClearButton())
+                        {
+                            buildFilePath = string.Empty;
+                        }
+                    EditorGUILayout.EndHorizontal();
+
+                    // - Build Profile -
+                    using(new EditorGUI.DisabledScope(!System.IO.File.Exists(buildFilePath)))
                     {
-                        UploadToServer();
+                        // - Version -
+                        EditorGUI.BeginChangeCheck();
+                            buildProfile.version.value = EditorGUILayout.TextField("Version",
+                                                                                   buildProfile.version.value);
+                        if(EditorGUI.EndChangeCheck())
+                        {
+                            buildProfile.version.isDirty = true;
+                        }
+                        // - Changelog -
+                        EditorGUI.BeginChangeCheck();
+                            EditorGUILayout.PrefixLabel("Changelog");
+                            buildProfile.changelog.value = EditorGUILayoutExtensions.MultilineTextField(buildProfile.changelog.value);
+                        if(EditorGUI.EndChangeCheck())
+                        {
+                            buildProfile.changelog.isDirty = true;
+                        }
+                        // - Metadata -
+                        EditorGUI.BeginChangeCheck();
+                            EditorGUILayout.PrefixLabel("Metadata");
+                            buildProfile.metadataBlob.value = EditorGUILayoutExtensions.MultilineTextField(buildProfile.metadataBlob.value);
+                        if(EditorGUI.EndChangeCheck())
+                        {
+                            buildProfile.metadataBlob.isDirty = true;
+                        }
                     }
-                    GUILayout.FlexibleSpace();
-                EditorGUILayout.EndHorizontal();
+
+                    // TODO(@jackson): if(profile) -> show build list?
+                    EditorGUILayout.Space();
+                    EditorGUILayout.BeginHorizontal();
+                        GUILayout.FlexibleSpace();
+                        if(GUILayout.Button("Upload to Server"))
+                        {
+                            UploadToServer();
+                        }
+                        GUILayout.FlexibleSpace();
+                    EditorGUILayout.EndHorizontal();
+                }
             }
         }
 
