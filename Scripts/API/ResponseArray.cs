eeb0725a4784c46f071d8b96bf3440cb491dab18
Jackson Wood <jackson@melodatron.com>
2018-09-19 16:34:39 +1000

    Updated ResponseArray for 2018-09-10 API Changes

--- Scripts/API/ResponseArray.cs
+++ Scripts/API/ResponseArray.cs
@@ -18,35 +18,26 @@ namespace ModIO.API
         [JsonProperty("result_offset")]
         private int _offset;
 
+        [JsonProperty("result_total")]
+        private int _total;
+
         [JsonProperty("data")]
         private T[] _items;
 
         // ---------[ ACCESSORS ]---------
-        /// <summary>
-        /// Number of results returned in the current request.
-        /// </summary>
+        /// <summary>Number of items in this response.</summary>
         public int Count    { get { return this._count; } }
 
-        /// <summary>
-        /// Maximum number of results returned.
-        /// </summary>
-        /// <remarks>
-        /// Defaults to 100 unless overridden by _limit.
-        /// </remarks>
+        /// <summary>Maximum number of results returned in this response.</summary>
         public int Limit    { get { return this._limit; } }
 
-
-        /// <summary>
-        /// Number of results skipped over.
-        /// </summary>
-        /// <remarks>
-        /// Defaults to 0 unless overridden by _offset.
-        /// </remarks>
+        /// <summary>Number of results skipped over.</summary>
         public int Offset   { get { return this._offset; } }
 
-        /// <summary>
-        /// The data returned by the request
-        /// </summary>
+        /// <summary>Total number of results on the mod.io servers.</summary>
+        public int Total    { get { return this._total; } }
+
+        /// <summary>The data returned by the request.</summary>
         public T[] Items    { get { return this._items; } }
 
         public T this[int index]
