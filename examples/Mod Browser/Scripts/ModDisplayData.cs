205e543a8a85e21df1f5135928839c82aed0595d
Jackson Wood <jackson@melodatron.com>
2018-12-15 15:18:21 +1100

    Added Get/SetLogo to ModDIsplayData

--- examples/Mod Browser/Scripts/ModDisplayData.cs
+++ examples/Mod Browser/Scripts/ModDisplayData.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace ModIO.UI
 {
     [System.Serializable]
@@ -9,6 +11,46 @@ namespace ModIO.UI
         public ModfileDisplayData       currentBuild;
         public ImageDisplayData[]       media;
         public ModTagDisplayData[]      tags;
+
+        public ImageDisplayData GetLogo()
+        {
+            if(media == null)
+            {
+                foreach(ImageDisplayData imageData in media)
+                {
+                    if(imageData.mediaType == ImageDisplayData.MediaType.ModLogo)
+                    {
+                        return imageData;
+                    }
+                }
+            }
+
+            return new ImageDisplayData();
+        }
+        public void SetLogo(ImageDisplayData value)
+        {
+            List<ImageDisplayData> mediaItems;
+            if(media == null)
+            {
+                mediaItems = new List<ImageDisplayData>();
+            }
+            else
+            {
+                mediaItems = new List<ImageDisplayData>(media);
+            }
+
+            for(int i = 0; i < mediaItems.Count; ++i)
+            {
+                ImageDisplayData imageData = mediaItems[i];
+                if(imageData.mediaType == ImageDisplayData.MediaType.ModLogo)
+                {
+                    mediaItems.RemoveAt(i);
+                    --i;
+                }
+            }
+
+            mediaItems.Insert(0, value);
+        }
     }
 
     public abstract class ModDisplayComponent : UnityEngine.MonoBehaviour
