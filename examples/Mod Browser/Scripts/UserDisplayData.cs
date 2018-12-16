008607b31ed980c52a784b5585082ef1f8cdb116
Jackson Wood <jackson@melodatron.com>
2018-12-16 17:04:10 +1100

    Created UserDisplayData

---
+++ examples/Mod Browser/Scripts/UserDisplayData.cs
@@ -0,0 +1,10 @@
+namespace ModIO.UI
+{
+    [System.Serializable]
+    public struct UserDisplayData
+    {
+        public int userId;
+        public UserProfileDisplayData profile;
+        public ImageDisplayData avatar;
+    }
+}
