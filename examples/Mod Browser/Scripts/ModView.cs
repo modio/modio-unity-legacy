b07b9c17842632d78d49c20ececd7cdd62b2502a
Jackson Wood <jackson@melodatron.com>
2018-12-18 00:15:08 +1100

    Added parsing for missing displays to ModView

--- examples/Mod Browser/Scripts/ModView.cs
+++ examples/Mod Browser/Scripts/ModView.cs
@@ -60,6 +60,9 @@ namespace ModIO.UI
         private delegate void DisplayProfileDelegate(ModProfile profile);
         private List<DisplayProfileDelegate> m_displayDelegates = new List<DisplayProfileDelegate>();
 
+        private delegate void ProfileParserDelegate(ModProfile profile, ref ModDisplayData data);
+        private List<ProfileParserDelegate> m_missingDisplayParsers = new List<ProfileParserDelegate>();
+
         private delegate void DisplayLoadingDelegate();
         private List<DisplayLoadingDelegate> m_loadingDelegates = new List<DisplayLoadingDelegate>();
 
@@ -119,7 +122,10 @@ namespace ModIO.UI
             m_getDelegates.Clear();
             m_setDelegates.Clear();
             m_displayDelegates.Clear();
+            m_missingDisplayParsers.Clear();
+            m_loadingDelegates.Clear();
 
+            // - profile -
             if(profileDisplay != null)
             {
                 profileDisplay.Initialize();
@@ -136,6 +142,15 @@ namespace ModIO.UI
                 m_displayDelegates.Add((p) => profileDisplay.DisplayProfile(p));
                 m_loadingDelegates.Add(() => profileDisplay.DisplayLoading());
             }
+            else
+            {
+                m_missingDisplayParsers.Add((ModProfile p, ref ModDisplayData d) =>
+                {
+                    d.profile = ModProfileDisplayData.CreateFromProfile(p);
+                });
+            }
+
+            // - media -
             if(mediaContainer != null)
             {
                 mediaContainer.Initialize();
@@ -152,6 +167,12 @@ namespace ModIO.UI
                 m_displayDelegates.Add((p) => mediaContainer.DisplayMedia(p));
                 m_loadingDelegates.Add(( ) => mediaContainer.DisplayLoading());
             }
+            else
+            {
+                m_missingDisplayParsers.Add(ParseProfileMedia);
+            }
+
+            // - logo -
             // NOTE(@jackson): Logo Data overrides Media Container Logo Data
             if(logoDisplay != null)
             {
@@ -169,6 +190,27 @@ namespace ModIO.UI
                 m_displayDelegates.Add((p) => logoDisplay.DisplayLogo(p.id, p.logoLocator));
                 m_loadingDelegates.Add(( ) => logoDisplay.DisplayLoading());
             }
+            else
+            {
+                m_missingDisplayParsers.Add((ModProfile p, ref ModDisplayData d) =>
+                {
+                    ImageDisplayData logoData = new ImageDisplayData()
+                    {
+                        modId = p.id,
+                        mediaType = ImageDisplayData.MediaType.ModLogo,
+                        fileName = string.Empty,
+                        texture = null,
+                    };
+                    if(p.logoLocator != null)
+                    {
+                        logoData.fileName = p.logoLocator.fileName;
+                    }
+
+                    d.SetLogo(logoData);
+                });
+            }
+
+            // - submittor -
             if(submittorDisplay.profile != null)
             {
                 submittorDisplay.profile.Initialize();
@@ -185,6 +227,14 @@ namespace ModIO.UI
                 m_displayDelegates.Add((p) => submittorDisplay.profile.DisplayProfile(p.submittedBy));
                 m_loadingDelegates.Add(( ) => submittorDisplay.profile.DisplayLoading());
             }
+            else
+            {
+                m_missingDisplayParsers.Add((ModProfile p, ref ModDisplayData d) =>
+                {
+                    d.submittorProfile = UserProfileDisplayData.CreateFromProfile(p.submittedBy);
+                });
+            }
+
             if(submittorDisplay.avatar != null)
             {
                 submittorDisplay.avatar.Initialize();
@@ -202,6 +252,32 @@ namespace ModIO.UI
                                                                                     p.submittedBy.avatarLocator));
                 m_loadingDelegates.Add(( ) => submittorDisplay.avatar.DisplayLoading());
             }
+            else
+            {
+                m_missingDisplayParsers.Add((ModProfile p, ref ModDisplayData d) =>
+                {
+                    ImageDisplayData avatarData = new ImageDisplayData()
+                    {
+                        userId = -1,
+                        mediaType = ImageDisplayData.MediaType.UserAvatar,
+                        fileName = string.Empty,
+                        texture = null,
+                    };
+                    if(p.submittedBy != null)
+                    {
+                        avatarData.userId = p.submittedBy.id;
+
+                        if(p.submittedBy.avatarLocator != null)
+                        {
+                            avatarData.fileName = p.submittedBy.avatarLocator.fileName;
+                        }
+                    }
+
+                    d.submittorAvatar = avatarData;
+                });
+            }
+
+            // - build -
             if(buildDisplay != null)
             {
                 buildDisplay.Initialize();
@@ -218,7 +294,16 @@ namespace ModIO.UI
                 m_displayDelegates.Add((p) => buildDisplay.DisplayModfile(p.currentBuild));
                 m_loadingDelegates.Add(( ) => buildDisplay.DisplayLoading());
             }
+            else
+            {
+                m_missingDisplayParsers.Add((ModProfile p, ref ModDisplayData d) =>
+                {
+                    d.currentBuild = ModfileDisplayData.CreateFromModfile(p.currentBuild);
+                });
+            }
 
+            // - tags -
+            // NOTE(@jackson): tags has no display/missing parse delegate as it requires categories
             if(tagsDisplay != null)
             {
                 tagsDisplay.Initialize();
@@ -232,10 +317,10 @@ namespace ModIO.UI
                     tagsDisplay.data = d.tags;
                 });
 
-                // NOTE(@jackson): tags has no display delegate as it requires categories
                 m_loadingDelegates.Add(( ) => tagsDisplay.DisplayLoading());
             }
 
+            // - stats -
             if(statisticsDisplay != null)
             {
                 statisticsDisplay.Initialize();
@@ -252,6 +337,7 @@ namespace ModIO.UI
                 m_loadingDelegates.Add(( ) => statisticsDisplay.DisplayLoading());
             }
 
+            // - download -
             if(downloadDisplay != null)
             {
                 downloadDisplay.Initialize();
@@ -267,6 +353,56 @@ namespace ModIO.UI
             }
         }
 
+        // NOTE(@jackson): This ignores the Logo as it'll be set anyway
+        private void ParseProfileMedia(ModProfile profile, ref ModDisplayData data)
+        {
+            // - early out -
+            if(profile.media == null)
+            {
+                data.media = new ImageDisplayData[0];
+                return;
+            }
+
+            // - parse -
+            List<ImageDisplayData> mediaList = new List<ImageDisplayData>();
+
+            if(profile.media.galleryImageLocators != null
+               && profile.media.galleryImageLocators.Length > 0)
+            {
+                foreach(GalleryImageLocator locator in profile.media.galleryImageLocators)
+                {
+                    ImageDisplayData imageData = new ImageDisplayData()
+                    {
+                        modId = profile.id,
+                        mediaType = ImageDisplayData.MediaType.ModGalleryImage,
+                        fileName = locator.fileName,
+                        texture = null,
+                    };
+
+                    mediaList.Add(imageData);
+                }
+            }
+
+            if(profile.media.youTubeURLs != null
+               && profile.media.youTubeURLs.Length > 0)
+            {
+                foreach(string url in profile.media.youTubeURLs)
+                {
+                    ImageDisplayData imageData = new ImageDisplayData()
+                    {
+                        modId = profile.id,
+                        mediaType = ImageDisplayData.MediaType.YouTubeThumbnail,
+                        youTubeId = Utility.ExtractYouTubeIdFromURL(url),
+                        texture = null,
+                    };
+
+                    mediaList.Add(imageData);
+                }
+            }
+
+            data.media = mediaList.ToArray();
+        }
+
         public void DisplayMod(ModProfile profile,
                                ModStatistics statistics,
                                IEnumerable<ModTagCategory> tagCategories,
@@ -275,39 +411,53 @@ namespace ModIO.UI
         {
             Debug.Assert(profile != null);
 
-            m_data = new ModDisplayData()
-            {
-                isSubscribed = isSubscribed,
-                isModEnabled = isModEnabled,
-            };
+            m_data = new ModDisplayData();
 
             foreach(DisplayProfileDelegate displayDelegate in m_displayDelegates)
             {
                 displayDelegate(profile);
             }
+            foreach(ProfileParserDelegate parserDelegate in m_missingDisplayParsers)
+            {
+                parserDelegate(profile, ref m_data);
+            }
 
+            // - tags -
             if(tagsDisplay != null)
             {
                 tagsDisplay.DisplayTags(profile, tagCategories);
             }
+            else
+            {
+                m_data.tags = ModTagDisplayData.GenerateArray(profile.tagNames, tagCategories);
+            }
+
+            // - stats -
+            ModStatisticsDisplayData statsData;
+            if(statistics == null)
+            {
+                statsData = new ModStatisticsDisplayData()
+                {
+                    modId = profile.id,
+                };
+            }
+            else
+            {
+                statsData = ModStatisticsDisplayData.CreateFromStatistics(statistics);
+            }
 
             if(statisticsDisplay != null)
             {
-                if(statistics == null)
-                {
-                    statisticsDisplay.data = new ModStatisticsDisplayData()
-                    {
-                        modId = profile.id,
-                    };
-                }
-                else
-                {
-                    statisticsDisplay.DisplayStatistics(statistics);
-                }
+                statisticsDisplay.data = statsData;
+            }
+            else
+            {
+                m_data.statistics = statsData;
             }
 
             // TODO(@jackson): DownloadDisplay
 
+            // - subscribed -
             if(subscriptionDisplay.isSubscribed != null)
             {
                 subscriptionDisplay.isSubscribed.SetActive(isSubscribed);
@@ -316,7 +466,9 @@ namespace ModIO.UI
             {
                 subscriptionDisplay.notSubscribed.SetActive(!isSubscribed);
             }
+            m_data.isSubscribed = isSubscribed;
 
+            // - enabled -
             if(modEnabledDisplay.isEnabled != null)
             {
                 modEnabledDisplay.isEnabled.SetActive(isModEnabled);
@@ -325,6 +477,15 @@ namespace ModIO.UI
             {
                 modEnabledDisplay.isDisabled.SetActive(!isModEnabled);
             }
+            m_data.isModEnabled = isModEnabled;
+
+            #if UNITY_EDITOR
+            if(Application.isPlaying)
+            {
+                // updates for inspection convenience
+                GetData();
+            }
+            #endif
         }
 
         public void DisplayLoading()
