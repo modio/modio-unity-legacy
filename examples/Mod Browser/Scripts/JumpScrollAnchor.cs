9822d5e16b659273eb0a94a2b8a5186859be0ba2
Jackson Wood <jackson@melodatron.com>
2018-12-19 03:01:47 +1100

    Completely rewrote JumpScrollRect...

--- examples/Mod Browser/Scripts/JumpScrollAnchor.cs
+++ examples/Mod Browser/Scripts/JumpScrollAnchor.cs
@@ -3,26 +3,5 @@ using UnityEngine;
 namespace ModIO.UI
 {
     [RequireComponent(typeof(RectTransform))]
-    public class JumpScrollAnchor : MonoBehaviour
-    {
-        private void OnEnable()
-        {
-            UpdateParentButtons();
-        }
-
-        private void OnDisable()
-        {
-            UpdateParentButtons();
-        }
-
-        private void UpdateParentButtons()
-        {
-            var scrollRectParents = this.GetComponentsInParent<JumpScrollRect>();
-
-            foreach(JumpScrollRect scrollRect in scrollRectParents)
-            {
-                scrollRect.UpdateButtonState();
-            }
-        }
-    }
+    public class JumpScrollAnchor : MonoBehaviour { }
 }
