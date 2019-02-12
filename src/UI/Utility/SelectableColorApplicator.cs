68e0ff1414ed7f9769359246528afb69e1db9743
Jackson Wood <jackson@melodatron.com>
2019-02-12 13:54:48 +1100

    Reimplemented SelectableColorApplicator with SelectableColorScheme

--- src/UI/Utility/SelectableColorApplicator.cs
+++ src/UI/Utility/SelectableColorApplicator.cs
@@ -4,21 +4,41 @@ using UnityEngine.UI;
 namespace ModIO.UI
 {
     [RequireComponent(typeof(Selectable))]
-    public class SelectableColorApplicator : AColorSchemeReceiver
+    public class SelectableColorApplicator : MonoBehaviour
     {
+        public SelectableColorScheme scheme = null;
+
         private Selectable selectable
         { get { return this.gameObject.GetComponent<Selectable>(); } }
 
-        public override void ApplyColorScheme(ColorScheme scheme)
+        public void UpdateColorScheme()
         {
-            selectable.colors = scheme.selectableColors;
+            if(scheme == null) { return; }
+
+            if(selectable.targetGraphic != null)
+            {
+                selectable.targetGraphic.color = scheme.imageColor;
+            }
+
+            selectable.colors = scheme.functionalColors;
         }
 
         #if UNITY_EDITOR
-        public override void ApplyColorScheme_withUndo(ColorScheme scheme)
+        public void UpdateColorScheme_withUndo()
         {
             UnityEditor.Undo.RecordObject(selectable, "Applied Color Scheme");
-            ApplyColorScheme(scheme);
+            UpdateColorScheme();
+        }
+
+        private void OnValidate()
+        {
+            UnityEditor.EditorApplication.delayCall += () =>
+            {
+                if(this != null)
+                {
+                    UpdateColorScheme_withUndo();
+                }
+            };
         }
         #endif
     }
