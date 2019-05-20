55626e652906f22094a4582c540c64ebd88abd36
Jackson Wood <jackson@melodatron.com>
2019-05-20 14:48:58 +1000

    OptionsMenu now handles show menu vs open profile

--- src/UI/OptionsMenu.cs
+++ src/UI/OptionsMenu.cs
@@ -8,69 +8,88 @@ namespace ModIO.UI
     {
         // ---------[ FIELDS ]---------
         [Header("UI Elements")]
+        /// <summary>Determines if the button behaviour changes if externally authenticated.</summary>
+        [Tooltip("If enabled, opens the browser for showHideButton.onClick if using Steam/GOG authentication.")]
+        public bool openBrowserIfExternalAuth = true;
         /// <summary>The button that can be clicked to show/hide the menu.</summary>
-        public Button showHideButton;
+        public Button showHideButton = null;
         /// <summary>The UI Element containing the menu elements.</summary>
-        public RectTransform dropdown;
+        public RectTransform dropdown = null;
         /// <summary>The menu option for viewing the player's profile on the mod.io website.</summary>
-        public Button viewProfileButton;
+        public Button viewProfileButton = null;
         /// <summary>The menu option that lets the player log out.</summary>
-        public Button logoutButton;
+        public Button logoutButton = null;
         /// <summary>The menu option that lets the player log in.</summary>
-        public Button loginButton;
+        public Button loginButton = null;
 
         // ---------[ INITIALIZATION ]---------
         private void Start()
         {
-            this.logoutButton.onClick.AddListener(Hide);
-            this.loginButton.onClick.AddListener(Hide);
+            this.showHideButton.onClick.AddListener(ToggleMenu);
+            this.logoutButton.onClick.AddListener(HideMenu);
+            this.loginButton.onClick.AddListener(HideMenu);
         }
 
         private void OnEnable()
         {
             this.dropdown.gameObject.SetActive(false);
-
-            OnUserAuthenticationDataUpdated();
         }
 
         // ---------[ EVENTS ]---------
-        /// <summary>Updates the menu to match the current UserAuthenticationData state.</summary>
-        public void OnUserAuthenticationDataUpdated()
+        /// <summary>Determines which functionality to use when the show/hide button is clicked.</summary>
+        public void ShowMenuOrOpenProfile()
         {
             UserAuthenticationData userData = UserAuthenticationData.instance;
-
-            // enable/disable log in/out buttons
             bool loggedIn = !(userData.Equals(UserAuthenticationData.NONE));
             bool isSteamAccount = (loggedIn && !string.IsNullOrEmpty(userData.steamTicket));
 
-            this.viewProfileButton.interactable = loggedIn;
-            this.loginButton.gameObject.SetActive(!loggedIn);
-            this.logoutButton.gameObject.SetActive(loggedIn && !isSteamAccount);
-
-            Text logoutButtonText = this.logoutButton.GetComponentInChildren<Text>();
-            if(logoutButtonText != null && loggedIn)
+            if(this.openBrowserIfExternalAuth && isSteamAccount)
             {
-                logoutButtonText.text = "Log out of " + userData.userId;
+                OpenProfileInBrowser();
+            }
+            else
+            {
+                ShowMenu();
             }
         }
 
         /// <summary>Shows the menu.</summary>
-        public void Show()
+        public void ShowMenu()
         {
+            UserAuthenticationData userData = UserAuthenticationData.instance;
+            bool loggedIn = !(userData.Equals(UserAuthenticationData.NONE));
+
+            this.logoutButton.gameObject.SetActive(loggedIn);
+            this.loginButton.gameObject.SetActive(!loggedIn);
+            this.viewProfileButton.gameObject.SetActive(loggedIn);
+
+            Text logoutButtonText = this.logoutButton.GetComponentInChildren<Text>();
+            if(logoutButtonText != null && loggedIn)
+            {
+                logoutButtonText.text = "Log out of account: " + userData.userId;
+            }
+
             this.dropdown.gameObject.SetActive(true);
         }
 
         /// <summary>Hides the menu.</summary>
-        public void Hide()
+        public void HideMenu()
         {
             this.dropdown.gameObject.SetActive(false);
         }
 
         /// <summary>Toggles the menu between show/hide.</summary>
-        public void Toggle()
+        public void ToggleMenu()
         {
             bool isActive = this.dropdown.gameObject.activeSelf;
-            this.dropdown.gameObject.SetActive(!isActive);
+            if(!isActive)
+            {
+                ShowMenuOrOpenProfile();
+            }
+            else
+            {
+                HideMenu();
+            }
         }
 
         // ---------[ MENU OPTIONS ]---------
