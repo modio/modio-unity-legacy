b6bec47e2fbd03558ff39d537f10aa6a5acdd2f7
Jackson Wood <jackson@melodatron.com>
2019-06-04 16:44:29 +1000

    Minor documentation added to ModStatisticsCache

--- src/UI/ModStatisticsCache.cs
+++ src/UI/ModStatisticsCache.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 
 namespace ModIO.UI
 {
+    /// <summary>A simple component for caching ModStatistics objects.</summary>
     public class ModStatisticsCache : MonoBehaviour
     {
+        // ---------[ FIELDS ]---------
+        /// <summary>Cached ModStatistics to id map.</summary>
         public Dictionary<int, ModStatistics> cache = new Dictionary<int, ModStatistics>();
 
+        // ---------[ ACCESSOR FUNCTIONS ]---------
+        /// <summary>Attempts to retrieve a cached ModStatistics object.</summary>
         public ModStatistics GetForId(int modId)
         {
             ModStatistics stats = null;
@@ -14,6 +19,7 @@ namespace ModIO.UI
             return stats;
         }
 
+        /// <summary>Stores a ModStatistics object in the cache.</summary>
         public void Store(IEnumerable<ModStatistics> statistics)
         {
             foreach(ModStatistics statData in statistics)
