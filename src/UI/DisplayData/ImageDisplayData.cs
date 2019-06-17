312a88fd2a6966d53fbf666a0e36dbd2ee689b28
Jackson Wood <jackson@melodatron.com>
2019-06-17 12:00:45 +1000

    Implemented URL fields in the ImageDisplayData

--- src/UI/DisplayData/ImageDisplayData.cs
+++ src/UI/DisplayData/ImageDisplayData.cs
@@ -22,6 +22,11 @@ namespace ModIO.UI
         public MediaType mediaType;
         public string imageId;
 
+        /// <summary>The URL for the original version of the image.</summary>
+        public string originalURL;
+        /// <summary>The URL for the thumbnail version of the image.</summary>
+        public string thumbnailURL;
+
         public Texture2D originalTexture;
         public Texture2D thumbnailTexture;
 
@@ -52,6 +57,81 @@ namespace ModIO.UI
             {
                 thumbnailTexture = value;
             }
+        }
+
+        /// <summary>Returns the image URL depending on whether the original or thumbnail is desired.</summary>
+        public string GetImageURL(bool original)
+        {
+            if(original){ return this.originalURL; }
+            else        { return this.thumbnailURL; }
+        }
+
+        // ---------[ GENERATION ]---------
+        /// <summary>Creates the ImageDisplayData for a mod logo.</summary>
+        public static ImageDisplayData CreateForModLogo(int modId, LogoImageLocator locator)
+        {
+            ImageDisplayData retVal = new ImageDisplayData()
+            {
+                ownerId = modId,
+                mediaType = MediaType.ModLogo,
+                imageId = locator.GetFileName(),
+                originalURL = locator.GetSizeURL(LogoSize.Original),
+                thumbnailURL = locator.GetSizeURL(ImageDisplayData.logoThumbnailSize),
+                originalTexture = null,
+                thumbnailTexture = null,
+            };
+
+            return retVal;
+        }
+        /// <summary>Creates the ImageDisplayData for a mod gallery image.</summary>
+        public static ImageDisplayData CreateForModGalleryImage(int modId, GalleryImageLocator locator)
+        {
+            ImageDisplayData retVal = new ImageDisplayData()
+            {
+                ownerId = modId,
+                mediaType = MediaType.ModGalleryImage,
+                imageId = locator.GetFileName(),
+                originalURL = locator.GetSizeURL(ModGalleryImageSize.Original),
+                thumbnailURL = locator.GetSizeURL(ImageDisplayData.galleryThumbnailSize),
+                originalTexture = null,
+                thumbnailTexture = null,
+            };
+
+            return retVal;
+        }
+        /// <summary>Creates the ImageDisplayData for a YouTube thumbnail.</summary>
+        public static ImageDisplayData CreateForYouTubeThumbnail(int modId, string youTubeId)
+        {
+            string url = Utility.GenerateYouTubeThumbnailURL(youTubeId);
+
+            ImageDisplayData retVal = new ImageDisplayData()
+            {
+                ownerId = modId,
+                mediaType = MediaType.YouTubeThumbnail,
+                imageId = youTubeId,
+                originalURL = url,
+                thumbnailURL = url,
+                originalTexture = null,
+                thumbnailTexture = null,
+            };
+
+            return retVal;
+        }
+        /// <summary>Creates the ImageDisplayData for a user avatar.</summary>
+        public static ImageDisplayData CreateForUserAvatar(int userId, AvatarImageLocator locator)
+        {
+            ImageDisplayData retVal = new ImageDisplayData()
+            {
+                ownerId = userId,
+                mediaType = MediaType.UserAvatar,
+                imageId = locator.GetFileName(),
+                originalURL = locator.GetSizeURL(UserAvatarSize.Original),
+                thumbnailURL = locator.GetSizeURL(ImageDisplayData.avatarThumbnailSize),
+                originalTexture = null,
+                thumbnailTexture = null,
+            };
+
+            return retVal;
         }
 
         // ---------[ EQUALITY OVERRIDES ]---------
