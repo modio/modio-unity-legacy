31aa01b757e991aae73018738da48c137e2e250d
Jackson Wood <jackson@melodatron.com>
2019-06-19 22:56:44 +1000

    Changed ImageRequest back to unmanaged class... for now

--- src/Requests/ImageRequest.cs
+++ src/Requests/ImageRequest.cs
@@ -1,6 +1,4 @@
 using System;
-using UnityEngine;
-using UnityEngine.Networking;
 
 using Texture2D = UnityEngine.Texture2D;
 
@@ -8,7 +6,6 @@ namespace ModIO
 {
     public class ImageRequest
     {
-        // ---------[ FIELDS ]---------
         public event Action<ImageRequest> succeeded;
         public event Action<ImageRequest> failed;
 
@@ -18,79 +15,15 @@ namespace ModIO
 
         public WebRequestError error;
 
-        private UnityWebRequestAsyncOperation m_asyncOperation;
-
-        // --- ACCESSORS ---
-        public UnityWebRequestAsyncOperation asyncOperation
-        {
-            get { return this.m_asyncOperation; }
-            set
-            {
-                if(this.m_asyncOperation != value)
-                {
-                    if(this.m_asyncOperation != null)
-                    {
-                        this.m_asyncOperation.completed -= this.OnCompleted;
-                    }
-                    if(value != null)
-                    {
-                        value.completed += this.OnCompleted;
-                    }
-
-                    this.m_asyncOperation = value;
-                }
-            }
-        }
-
-        // ---------[ EVENTS ]---------
-        private void OnCompleted(AsyncOperation operation)
-        {
-            Debug.Assert(operation == this.m_asyncOperation);
-
-            UnityWebRequest webRequest = this.m_asyncOperation.webRequest;
-            this.isDone = true;
-
-            if(webRequest.isNetworkError || webRequest.isHttpError)
-            {
-                this.error = WebRequestError.GenerateFromWebRequest(webRequest);
-
-                if(this.failed != null)
-                {
-                    this.failed(this);
-                }
-            }
-            else
-            {
-                #if DEBUG
-                if(PluginSettings.data.logAllRequests)
-                {
-                    var responseTimeStamp = ServerTimeStamp.Now;
-                    Debug.Log("IMAGE DOWNLOAD SUCEEDED"
-                              + "\nDownload completed at: " + ServerTimeStamp.ToLocalDateTime(responseTimeStamp)
-                              + "\nURL: " + webRequest.url);
-                }
-                #endif
-
-                this.imageTexture = (webRequest.downloadHandler as DownloadHandlerTexture).texture;
-
-                if(succeeded != null)
-                {
-                    succeeded(this);
-                }
-            }
-        }
-
-        // ---------[ OBSOLETE ]---------
-        [Obsolete("No longer necessary.")]
-        public void NotifySucceeded()
+        internal void NotifySucceeded()
         {
             if(succeeded != null)
             {
                 succeeded(this);
             }
         }
-        [Obsolete("No longer necessary.")]
-        public void NotifyFailed()
+
+        internal void NotifyFailed()
         {
             if(failed != null)
             {
