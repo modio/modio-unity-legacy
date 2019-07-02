30eae48ca2cb79919d21e780d417367706ab19bd
Jackson Wood <jackson@melodatron.com>
2019-07-02 18:35:33 +1000

    Implemented IModViewElement

---
+++ src/UI/IModViewElement.cs
@@ -0,0 +1,8 @@
+namespace ModIO.UI
+{
+    /// <summary>Allows a MonoBehaviour to be assigned to a ModView.</summary>
+    public interface IModViewElement
+    {
+        void SetModView(ModView view);
+    }
+}
