62ed9fe09a2156384d0c4dfc3e64c009d60d391f
Jackson Wood <jackson@melodatron.com>
2019-07-19 20:29:18 +1000

    Added IUserViewElement

---
+++ src/UI/IUserViewElement.cs
@@ -0,0 +1,8 @@
+namespace ModIO.UI
+{
+    /// <summary>Allows a MonoBehaviour to be assigned to a UserView.</summary>
+    public interface IUserViewElement
+    {
+        void SetUserView(UserView view);
+    }
+}
