ec42d7067494f06ce9171d4e2149acb2879bae84
Jackson Wood <jackson@melodatron.com>
2019-07-19 19:09:12 +1000

    Implemented TextureAspectRatioMatcher

---
+++ src/UI/DisplayComponents/TextureAspectRatioMatcher.cs
@@ -0,0 +1,31 @@
+using UnityEngine.UI;
+
+namespace ModIO.UI
+{
+    /// <summary>Variant of the AspectRatioFitter that can match the ratio of a given texture.</summary>
+    public class TextureAspectRatioMatcher : AspectRatioFitter
+    {
+        /// <summary>Sets the aspect ratio to match the given texture.</summary>
+        public void MatchTexture(UnityEngine.Texture2D texture)
+        {
+            if(texture != null)
+            {
+                this.aspectRatio = ((float)texture.width / (float)texture.height);
+            }
+        }
+
+        #if UNITY_EDITOR
+        /// <summary>Solves an issue where updates throw a warning in-editor.</summary>
+        protected override void OnValidate()
+        {
+            UnityEditor.EditorApplication.delayCall += () =>
+            {
+                if(this != null)
+                {
+                    base.OnValidate();
+                }
+            };
+        }
+        #endif
+    }
+}
