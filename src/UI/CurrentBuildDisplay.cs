072c29f30da98e286f4d4cca1a157c5eec1adf38
Jackson Wood <jackson@melodatron.com>
2019-07-22 12:16:46 +1000

    Implemented CurrentBuildDisplay

---
+++ src/UI/CurrentBuildDisplay.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace ModIO.UI
+{
+    /// <summary>A display component that pairs a ModfileView with a ModView to display the current build.</summary>
+    [RequireComponent(typeof(ModfileView))]
+    public class CurrentBuildDisplay : MonoBehaviour, IModViewElement
+    {
+        // ---------[ FIELDS ]---------
+        /// <summary>Parent ModView.</summary>
+        private ModView m_view = null;
+
+        // ---------[ INITIALIZATION ]---------
+        /// <summary>IModViewElement interface.</summary>
+        public void SetModView(ModView view)
+        {
+            // early out
+            if(this.m_view == view) { return; }
+
+            // unhook
+            if(this.m_view != null)
+            {
+                this.m_view.onProfileChanged -= DisplayCurrentBuild;
+            }
+
+            // assign
+            this.m_view = view;
+
+            // hook
+            if(this.m_view != null)
+            {
+                this.m_view.onProfileChanged += DisplayCurrentBuild;
+                this.DisplayCurrentBuild(this.m_view.profile);
+            }
+            else
+            {
+                this.DisplayCurrentBuild(null);
+            }
+        }
+
+        // ---------[ UI FUNCTIONALITY ]---------
+        /// <summary>Displays the current build for a ModProfile.</summary>
+        public void DisplayCurrentBuild(ModProfile modProfile)
+        {
+            Modfile modfile = null;
+            if(modProfile != null)
+            {
+                modfile = modProfile.currentBuild;
+            }
+
+            this.gameObject.GetComponent<ModfileView>().modfile = modfile;
+        }
+    }
+}
