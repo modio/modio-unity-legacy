e74297f6a9eb2847ded7f1ea5034a310ae76a9b8
Jackson Wood <jackson@melodatron.com>
2019-07-22 13:15:51 +1000

    Reverted changes to ModTagCollectionTextDisplay

--- src/UI/DisplayComponents/ModTagCollectionTextDisplay.cs
+++ src/UI/DisplayComponents/ModTagCollectionTextDisplay.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 
 namespace ModIO.UI
 {
-    /// <summary>Displays the tags of a mod in a single text component.</summary>
-    public class ModTagCollectionTextDisplay : MonoBehaviour, IModViewElement
+    [System.Obsolete("Use TagCollectionTextDisplay instead.")]
+    [RequireComponent(typeof(Text))]
+    public class ModTagCollectionTextDisplay : ModTagCollectionDisplayComponent
     {
         // ---------[ FIELDS ]---------
+        public event System.Action<ModTagCollectionDisplayComponent> onClick;
+
         [Header("Settings")]
         public bool includeCategory = false;
         public string tagSeparator = ", ";
@@ -20,14 +23,10 @@ namespace ModIO.UI
         [Header("Display Data")]
         [SerializeField] private ModTagDisplayData[] m_data = new ModTagDisplayData[0];
 
-        /// <summary>Wrapper for the text component.</summary>
-        private GenericTextComponent m_textComponent = new GenericTextComponent();
-
-        /// <summary>Parent ModView.</summary>
-        private ModView m_view = null;
-
         // --- ACCESSORS ---
-        public IEnumerable<ModTagDisplayData> data
+        public Text text { get { return this.gameObject.GetComponent<Text>(); } }
+
+        public override IEnumerable<ModTagDisplayData> data
         {
             get { return m_data; }
             set
@@ -66,7 +65,8 @@ namespace ModIO.UI
                 builder.Length -= tagSeparator.Length;
             }
 
-            this.m_textComponent.text = builder.ToString();
+            text.text = builder.ToString();
+            text.enabled = true;
 
             if(loadingOverlay != null)
             {
@@ -74,71 +74,23 @@ namespace ModIO.UI
             }
         }
 
-        // ---------[ INITIALIZATION ]---------
-        protected virtual void Awake()
+        // ---------[ INITIALIZE ]---------
+        public override void Initialize()
         {
-            Component textDisplayComponent = GenericTextComponent.FindCompatibleTextComponent(this.gameObject);
-            this.m_textComponent.SetTextDisplayComponent(textDisplayComponent);
-
-            #if DEBUG
-            if(textDisplayComponent == null)
+            if(Application.isPlaying)
             {
-                Debug.LogWarning("[mod.io] No compatible text components were found on this "
-                                 + "GameObject to set text for."
-                                 + "\nCompatible components are UnityEngine.UI.Text, "
-                                 + "UnityEngine.TextMesh, and components derived from TMPro.TMP_Text.",
-                                 this);
-            }
-            #endif
-        }
-        // ---------[ INITIALIZATION ]---------
-        /// <summary>IModViewElement interface.</summary>
-        public void SetModView(ModView view)
-        {
-            // early out
-            if(this.m_view == view) { return; }
-
-            // unhook
-            if(this.m_view != null)
-            {
-                this.m_view.onProfileChanged -= DisplayProfileTags;
-            }
-
-            // assign
-            this.m_view = view;
-
-            // hook
-            if(this.m_view != null)
-            {
-                this.m_view.onProfileChanged += DisplayProfileTags;
-                this.DisplayProfileTags(this.m_view.profile);
-            }
-            else
-            {
-                this.DisplayProfileTags(null);
+                Debug.Assert(text != null);
             }
         }
 
-        // ---------[ UI FUNCTIONALITY ]---------
-        /// <summary>Displays the tags for a given profile.</summary>
-        public void DisplayProfileTags(ModProfile profile)
-        {
-            IEnumerable<string> tags = null;
-            if(profile != null)
-            {
-                tags = profile.tagNames;
-            }
-
-            this.DisplayTags(tags, null);
-        }
-
-        public void DisplayTags(ModProfile profile, IEnumerable<ModTagCategory> tagCategories)
+        // ---------[ UI FUNCTIONALITY ]--------
+        public override void DisplayTags(ModProfile profile, IEnumerable<ModTagCategory> tagCategories)
         {
             Debug.Assert(profile != null);
             this.DisplayTags(profile.tagNames, tagCategories);
         }
 
-        public void DisplayTags(IEnumerable<string> tags, IEnumerable<ModTagCategory> tagCategories)
+        public override void DisplayTags(IEnumerable<string> tags, IEnumerable<ModTagCategory> tagCategories)
         {
             if(tags == null)
             {
@@ -149,9 +101,9 @@ namespace ModIO.UI
             PresentData(m_data);
         }
 
-        public void DisplayLoading()
+        public override void DisplayLoading()
         {
-            this.m_textComponent.text = string.Empty;
+            text.text = string.Empty;
 
             if(loadingOverlay != null)
             {
@@ -160,6 +112,14 @@ namespace ModIO.UI
         }
 
         // ---------[ EVENT HANDLING ]---------
+        public void NotifyClicked()
+        {
+            if(this.onClick != null)
+            {
+                this.onClick(this);
+            }
+        }
+
         #if UNITY_EDITOR
         private void OnValidate()
         {
@@ -172,15 +132,5 @@ namespace ModIO.UI
             };
         }
         #endif
-
-        // ---------[ OBSOLETE ]---------
-        [System.Obsolete("No longer available.")]
-        public event System.Action<ModTagCollectionDisplayComponent> onClick;
-
-        [System.Obsolete("No longer necessary.")]
-        public void Initialize() {}
-
-        [System.Obsolete("No longer available.")]
-        public void NotifyClicked() {}
     }
 }
