70ba556b0258abb3cc5330747ce1287878029712
Jackson Wood <jackson@melodatron.com>
2019-07-23 14:08:47 +1000

    Implemented ObjectActiveSetter

---
+++ src/UI/Utility/ObjectActiveSetter.cs
@@ -0,0 +1,12 @@
+namespace ModIO.UI
+{
+    /// <summary>Component that can be used to activate/deactivate the GameObject attached to it.</summary>
+    public class ObjectActiveSetter : UnityEngine.MonoBehaviour
+    {
+        /// <summary>Checks to see if an object is null.</summary>
+        public void ActiveOnlyIfNull(object o)
+        {
+            this.gameObject.SetActive(o == null);
+        }
+    }
+}
