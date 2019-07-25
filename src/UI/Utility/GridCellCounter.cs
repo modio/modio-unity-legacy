494f15332a75d3bdad4413e71fd21a13a3c895e8
Jackson Wood <jackson@melodatron.com>
2019-07-25 15:10:36 +1000

    Implemented GridCellCounter

---
+++ src/UI/Utility/GridCellCounter.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ModIO.UI
+{
+    /// <summary>Fires an event when the count of visible grid cells changes.</summary>
+    [RequireComponent(typeof(GridLayoutGroup))]
+    public class GridCellCounter : MonoBehaviour
+    {
+        // ---------[ NESTED DATA-TYPES ]---------
+        /// <summary>Event fired when the number of visible cells changes.</summary>
+        [System.Serializable]
+        public class CellCountChanged : UnityEngine.Events.UnityEvent<int> {}
+
+        // ---------[ FIELDS ]---------
+        /// <summary>Event fired when the number of visible cells changes.</summary>
+        public CellCountChanged onCellCountChanged = null;
+
+        /// <summary>Dimensions last update.</summary>
+        private Rect m_lastDimensions = new Rect() { x = -1, y = -1 };
+
+        /// <summary>Cell count last update.</summary>
+        private int m_lastCellCount = -1;
+
+        // --- Accessors ---
+        /// <summary>Grid Layout component being referenced.</summary>
+        public GridLayoutGroup grid
+        {
+            get { return this.GetComponent<GridLayoutGroup>(); }
+        }
+
+        /// <summary>RectTransform component attached to the game object.</summary>
+        private RectTransform rectTransform
+        {
+            get { return (RectTransform)this.transform; }
+        }
+
+        // ---------[ INITIALIZATION ]---------
+        private void Start()
+        {
+            this.OnGUI();
+        }
+
+        private void OnEnable()
+        {
+            this.OnGUI();
+        }
+
+        private void OnGUI()
+        {
+            if(this.m_lastDimensions != this.rectTransform.rect)
+            {
+                int newCount = UIUtilities.CountVisibleGridCells(this.grid);
+                if(newCount != this.m_lastCellCount
+                   && this.onCellCountChanged != null)
+                {
+                    this.onCellCountChanged.Invoke(newCount);
+                }
+
+                this.m_lastCellCount = newCount;
+                this.m_lastDimensions = this.rectTransform.rect;
+            }
+        }
+    }
+}
