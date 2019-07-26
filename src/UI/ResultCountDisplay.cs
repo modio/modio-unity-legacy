0f288b65d78e8dfd8532a139836648227f05a660
Jackson Wood <jackson@melodatron.com>
2019-07-26 12:42:15 +1000

    Implemented ResultCountDisplay

---
+++ src/UI/ResultCountDisplay.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+
+namespace ModIO.UI
+{
+    /// <summary>Displays the number of results for the view.</summary>
+    public class ResultCountDisplay : MonoBehaviour, ISubscriptionsViewElement
+    {
+        // ---------[ FIELDS ]---------
+        /// <summary>Wrapper for the text component.</summary>
+        private GenericTextComponent m_textComponent = new GenericTextComponent();
+
+        /// <summary>Parent SubscriptionsView.</summary>
+        private SubscriptionsView m_subsView = null;
+
+        /// <summary>Currently displayed count.</summary>
+        private int m_resultCount = 0;
+
+        // ---------[ INITIALIZATION ]---------
+        /// <summary>Collect text component.</summary>
+        protected virtual void Awake()
+        {
+            Component textDisplayComponent = GenericTextComponent.FindCompatibleTextComponent(this.gameObject);
+            this.m_textComponent.SetTextDisplayComponent(textDisplayComponent);
+
+            #if DEBUG
+            if(textDisplayComponent == null)
+            {
+                Debug.LogWarning("[mod.io] No compatible text components were found on this "
+                                 + "GameObject to set text for."
+                                 + "\nCompatible components are UnityEngine.UI.Text, "
+                                 + "UnityEngine.TextMesh, and components derived from TMPro.TMP_Text.",
+                                 this);
+            }
+            #endif
+        }
+
+        /// <summary>Assert display is current.</summary>
+        protected virtual void OnEnable()
+        {
+            this.m_textComponent.text = m_resultCount.ToString();
+        }
+
+        /// <summary>ISubscriptionsViewElement interface.</summary>
+        public void SetSubscriptionsView(SubscriptionsView view)
+        {
+            // early out
+            if(this.m_subsView == view) { return; }
+
+            // unhook
+            if(this.m_subsView != null)
+            {
+                this.m_subsView.onModPageChanged.RemoveListener(DisplayPageTotal);
+            }
+
+            // assign
+            this.m_subsView = view;
+
+            // hook
+            if(this.m_subsView != null)
+            {
+                this.m_subsView.onModPageChanged.AddListener(DisplayPageTotal);
+                this.DisplayPageTotal(this.m_subsView.displayedMods);
+            }
+            else
+            {
+                this.DisplayPageTotal(null);
+            }
+        }
+
+        // ---------[ UI FUNCTIONALITY ]---------
+        /// <summary>Gets the value to display from a RequestPage.</summary>
+        public void DisplayPageTotal(RequestPage<ModProfile> page)
+        {
+            this.m_resultCount = 0;
+            if(page != null)
+            {
+                this.m_resultCount = page.resultTotal;
+            }
+
+            if(this.isActiveAndEnabled)
+            {
+                this.m_textComponent.text = this.m_resultCount.ToString();
+            }
+        }
+    }
+}
