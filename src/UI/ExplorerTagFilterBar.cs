8446e1e0c2db9d74534693e5df8f1afc19a65cd5
Jackson Wood <jackson@melodatron.com>
2019-08-12 14:58:47 +1000

    Reworked to be compatible with TagContainer

--- src/UI/ExplorerTagFilterBar.cs
+++ src/UI/ExplorerTagFilterBar.cs
@@ -4,35 +4,25 @@ using UnityEngine;
 
 namespace ModIO.UI
 {
-    [RequireComponent(typeof(ModTagContainer))]
-    public class ExplorerTagFilterBar : MonoBehaviour, IGameProfileUpdateReceiver
+    [RequireComponent(typeof(TagContainer))]
+    public class ExplorerTagFilterBar : MonoBehaviour
     {
         // ---------[ FIELDS ]---------
         /// <summary>ExplorerView to set the tagFilter on.</summary>
         public ExplorerView view = null;
-        /// <summary>Should this object be disabled if there are no tags.</summary>
-        public bool hideIfEmpty = true;
 
-        /// <summary>Mod Tag Categories for displaying tag data.</summary>
-        private ModTagCategory[] m_tagCategories = new ModTagCategory[0];
         /// <summary>Tags to display as selected.</summary>
         private List<string> m_selectedTags = new List<string>();
 
         // --- ACCESSORS ---
-        public ModTagContainer container
+        public TagContainer container
         {
-            get { return this.gameObject.GetComponent<ModTagContainer>(); }
+            get { return this.gameObject.GetComponent<TagContainer>(); }
         }
 
         // ---------[ INITIALIZATION ]---------
         private void Start()
         {
-            container.Initialize();
-            container.tagClicked += (display) =>
-            {
-                view.RemoveTagFromFilter(display.data.tagName);
-            };
-
             // init tag selection
             this.view.onTagFilterUpdated += (t) =>
             {
@@ -40,36 +30,17 @@ namespace ModIO.UI
                 this.Refresh();
             };
             this.m_selectedTags = new List<string>(this.view.GetTagFilter());
-
-            // init tag categories
-            var tagCategories = ModBrowser.instance.gameProfile.tagCategories;
-            if(tagCategories != null)
-            {
-                this.m_tagCategories = tagCategories;
-            }
-
-            // update display
             this.Refresh();
         }
 
         // ---------[ DISPLAY FUNCTIONALITY ]---------
         public void Refresh()
         {
-            this.container.DisplayTags(this.m_selectedTags, this.m_tagCategories);
-            this.gameObject.SetActive(!hideIfEmpty || this.m_selectedTags.Count > 0);
+            this.container.DisplayTags(this.m_selectedTags);
         }
 
-        // ---------[ EVENTS ]---------
-        /// <summary>React to game profile update message.</summary>
-        public void OnGameProfileUpdated(GameProfile gameProfile)
-        {
-            Debug.Assert(gameProfile != null);
-
-            if(this.m_tagCategories != gameProfile.tagCategories)
-            {
-                this.m_tagCategories = gameProfile.tagCategories;
-                this.Refresh();
-            }
-        }
+        // ---------[ OBSOLETE ]---------
+        [System.Obsolete("Use TagContainer.hideIfEmpty instead.")][HideInInspector]
+        public bool hideIfEmpty;
     }
 }
