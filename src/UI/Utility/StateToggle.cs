3aff7037a0fc9e134837e92bb6b0d889e50d8c4c
Jackson Wood <jackson@melodatron.com>
2019-08-20 17:45:06 +1000

    Implemented StateToggle

---
+++ src/UI/Utility/StateToggle.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ModIO.UI
+{
+    /// <summary>A wrapper for the Unity UI Toggle component to allow it to be used as a StateToggleDisplay.</summary>
+    [RequireComponent(typeof(Toggle))]
+    public class StateToggle : StateToggleDisplay
+    {
+        /// <summary>Pass-through to the Toggle sibling component.</summary>
+        public override bool isOn
+        {
+            get { return this.gameObject.GetComponent<Toggle>().isOn; }
+            set { this.gameObject.GetComponent<Toggle>().isOn = value; }
+        }
+    }
+}
