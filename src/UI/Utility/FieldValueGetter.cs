325bdc289b760804c823fe5e44644ba983c56a27
Jackson Wood <jackson@melodatron.com>
2019-08-29 11:04:26 +1000

    Implemented FieldSelectionDisplayAttribute

--- src/UI/Utility/FieldValueGetter.cs
+++ src/UI/Utility/FieldValueGetter.cs
@@ -5,33 +5,59 @@ using UnityEngine;
 
 namespace ModIO.UI
 {
+    /// <summary>Property to use for the custom property drawer.</summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class FieldSelectionDisplayAttribute : PropertyAttribute
+    {
+        /// <summary>Type to use for reflection.</summary>
+        public Type objectType = null;
+        public bool displayArrays = false;
+        public bool displayNested = false;
+
+        public FieldSelectionDisplayAttribute(Type objectType, bool displayArrays = false, bool displayNested = false)
+        {
+            this.objectType = objectType;
+            this.displayArrays = displayArrays;
+            this.displayNested = displayNested;
+        }
+    }
+
     /// <summary>Structure to use for displaying the field of a piece of data.</summary>
     [System.Serializable]
     public struct FieldValueGetter
     {
+        // ---------[ FIELDS ]---------
         /// <summary>Field of the object to display.</summary>
         [SerializeField]
-        private string fieldPath;
+        private string m_fieldPath;
 
         /// <summary>Field info for fetching the desired value.</summary>
-        private FieldInfo[] fieldInfo;
+        private FieldInfo[] m_fieldInfo;
+
+        // ---------[ INITIALIZATION ]---------
+        /// <summary>Initialization</summary>
+        public FieldValueGetter(string fieldPath = null)
+        {
+            this.m_fieldPath = fieldPath;
+            this.m_fieldInfo = null;
+        }
 
         /// <summary>Returns the value stored at the given field path.</summary>
         public object GetValue(object objectInstance)
         {
             if(objectInstance == null) { return null; }
 
-            if(this.fieldInfo == null)
+            if(this.m_fieldInfo == null)
             {
-                this.fieldInfo = FieldValueGetter.GenerateFieldInfo(objectInstance.GetType(), this.fieldPath);
+                this.m_fieldInfo = FieldValueGetter.GenerateFieldInfo(objectInstance.GetType(), this.m_fieldPath);
             }
 
-            if(this.fieldInfo.Length > 0)
+            if(this.m_fieldInfo.Length > 0)
             {
                 object lastObject = objectInstance;
-                for(int i = 0; i < fieldInfo.Length && lastObject != null; ++i)
+                for(int i = 0; i < this.m_fieldInfo.Length && lastObject != null; ++i)
                 {
-                    lastObject = fieldInfo[i].GetValue(lastObject);
+                    lastObject = this.m_fieldInfo[i].GetValue(lastObject);
                 }
 
                 if(lastObject != null)
