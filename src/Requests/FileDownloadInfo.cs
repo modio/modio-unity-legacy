ff97307eadfa011c57f6df1e89e0ec8b74528d55
Jackson Wood <jackson@melodatron.com>
2019-09-11 12:45:17 +1000

    Added bytesPerSecond

--- src/Requests/FileDownloadInfo.cs
+++ src/Requests/FileDownloadInfo.cs
@@ -4,10 +4,16 @@ namespace ModIO
     public class FileDownloadInfo
     {
         public UnityEngine.Networking.UnityWebRequest request;
+
         public WebRequestError error;
         public string target;
         public System.Int64 fileSize;
         public bool isDone;
         public bool wasAborted;
+
+        /// <summary>Number of bytes being downloaded per-second.</summary>
+        /// <remarks>This value is *UNRELIABLE* as it currently only being
+        /// updated by UI components. This will be fixed in the next release.</remarks>
+        public System.Int64 bytesPerSecond;
     }
 }
