7390d4cdacb06a578f0ccc38215b9f6374d54280
Jackson Wood <jackson@melodatron.com>
2019-09-12 15:41:04 +1000

    Implemented MemberReference and custom editor as replacement for FieldValueGetter

---
+++ src/UI/Editor/MemberReferenceDropdownDrawer.cs
@@ -0,0 +1,246 @@
+#if UNITY_EDITOR
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+using UnityEditor;
+using UnityEngine;
+
+namespace ModIO.UI.EditorCode
+{
+    /// <summary>Draws a dropdown selection in place of the MemberReference.</summary>
+    [CustomPropertyDrawer(typeof(MemberReference.DropdownDisplayAttribute))]
+    public class MemberReferenceDropdownDrawer : PropertyDrawer
+    {
+        // ---------[ NESTED DATA-TYPES ]---------
+        /// <summary>A structure for pairing the MemberInfo with a path.</summary>
+        private struct MemberInfoPath
+        {
+            public string pathPrefix;
+            public Type memberType;
+            public MemberInfo info;
+        }
+
+        // ---------[ FIELDS ]---------
+        /// <summary>Is the dropdown open?</summary>
+        public bool isExpanded = false;
+
+        /// <summary>Field Path property.</summary>
+        public SerializedProperty memberPathProperty = null;
+
+        /// <summary>DisplayValues for the popup.</summary>
+        public string[] displayValues = null;
+
+        // ---------[ GUI FUNCTIONALITY ]---------
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // Generate Display Values
+            if(this.memberPathProperty == null
+               || this.displayValues == null)
+            {
+                MemberReference.DropdownDisplayAttribute dropdownAttribute
+                    = (MemberReference.DropdownDisplayAttribute)attribute;
+
+                this.memberPathProperty = property.FindPropertyRelative("m_memberPath");
+
+                Debug.Assert(dropdownAttribute != null);
+
+                // Generate member info
+                FieldInfo[] rootFields
+                    = dropdownAttribute.objectType.GetFields(BindingFlags.Instance | BindingFlags.Public);
+
+                PropertyInfo[] rootProperties
+                    = dropdownAttribute.objectType.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+                string[] membersToIgnore = dropdownAttribute.membersToIgnore;
+                if(membersToIgnore == null)
+                {
+                    membersToIgnore = new string[0];
+                }
+
+                List<MemberInfoPath> memberPaths = GenerateMemberInfoPathList(rootFields,
+                                                                              rootProperties,
+                                                                              membersToIgnore,
+                                                                              dropdownAttribute.displayEnumerables,
+                                                                              dropdownAttribute.displayNested,
+                                                                              string.Empty);
+
+                // generate strings
+                List<string> displayNames = new List<string>(memberPaths.Count);
+                for(int i = 0; i < memberPaths.Count; ++i)
+                {
+                    displayNames.Add(memberPaths[i].pathPrefix + memberPaths[i].info.Name);
+                }
+
+                displayNames.Sort();
+
+                this.displayValues = displayNames.ToArray();
+            }
+
+            // Get Selection
+            int selectedIndex = -1;
+            string selection = memberPathProperty.stringValue;
+
+            foreach(string memberPath in this.displayValues)
+            {
+                ++selectedIndex;
+
+                if(selection == memberPath) { break; }
+            }
+
+            if(selectedIndex > this.displayValues.Length)
+            {
+                selectedIndex = 0;
+            }
+
+            selectedIndex = EditorGUI.Popup(position, "Target Member", selectedIndex, this.displayValues);
+            this.memberPathProperty.stringValue = this.displayValues[selectedIndex];
+        }
+
+        // ---------[ UTILITY ]---------
+        /// <summary>Generates the MemberInfoPath data for the given MemberInfo.</summary>
+        private static List<MemberInfoPath> GenerateMemberInfoPathList(FieldInfo[] fieldInfoArray,
+                                                                       PropertyInfo[] propertyInfoArray,
+                                                                       string[] membersToIgnore,
+                                                                       bool displayEnumerables,
+                                                                       bool displayNested,
+                                                                       string pathPrefix)
+        {
+            List<MemberInfoPath> retVal = new List<MemberInfoPath>();
+            List<MemberInfoPath> nested = new List<MemberInfoPath>();
+
+            // process root members
+            foreach(FieldInfo field in fieldInfoArray)
+            {
+                // obsolete check
+                if(Attribute.IsDefined(field, typeof(ObsoleteAttribute)))
+                {
+                    continue;
+                }
+
+                // enumerable check
+                if(!displayEnumerables
+                   && MemberReferenceDropdownDrawer.IsTypeEnumerable(field.FieldType))
+                {
+                    continue;
+                }
+
+                // mask check
+                string fullName = pathPrefix + field.Name;
+                bool skipMember = false;
+                for(int i = 0; i < membersToIgnore.Length && !skipMember; ++i)
+                {
+                    skipMember = (membersToIgnore[i] == fullName);
+                }
+                if(skipMember)
+                {
+                    continue;
+                }
+
+                // add to list
+                MemberInfoPath mip = new MemberInfoPath()
+                {
+                    pathPrefix = pathPrefix,
+                    memberType = field.FieldType,
+                    info = field,
+                };
+                retVal.Add(mip);
+
+                // add nested?
+                if(displayNested
+                   && field.FieldType.IsClass
+                   && field.FieldType != typeof(string))
+                {
+                    nested.Add(mip);
+                }
+            }
+            // process root members
+            foreach(PropertyInfo property in propertyInfoArray)
+            {
+                // obsolete check
+                if(Attribute.IsDefined(property, typeof(ObsoleteAttribute)))
+                {
+                    continue;
+                }
+
+                // enumerable check
+                if(!displayEnumerables
+                   && MemberReferenceDropdownDrawer.IsTypeEnumerable(property.PropertyType))
+                {
+                    continue;
+                }
+
+                // mask check
+                string fullName = pathPrefix + property.Name;
+                bool skipMember = false;
+                for(int i = 0; i < membersToIgnore.Length && !skipMember; ++i)
+                {
+                    skipMember = (membersToIgnore[i] == fullName);
+                }
+                if(skipMember)
+                {
+                    continue;
+                }
+
+                // add to list
+                MemberInfoPath mip = new MemberInfoPath()
+                {
+                    pathPrefix = pathPrefix,
+                    memberType = property.PropertyType,
+                    info = property,
+                };
+                retVal.Add(mip);
+
+                // add nested?
+                if(displayNested
+                   && property.PropertyType.IsClass
+                   && property.PropertyType != typeof(string))
+                {
+                    nested.Add(mip);
+                }
+            }
+
+            // process nested
+            foreach(MemberInfoPath nestedInfo in nested)
+            {
+                string prefix = pathPrefix + nestedInfo.info.Name + ".";
+
+                FieldInfo[] nestedFields
+                    = nestedInfo.memberType.GetFields(BindingFlags.Instance | BindingFlags.Public);
+                PropertyInfo[] nestedProperties
+                    = nestedInfo.memberType.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+                retVal.AddRange(GenerateMemberInfoPathList(nestedFields,
+                                                           nestedProperties,
+                                                           membersToIgnore,
+                                                           displayEnumerables,
+                                                           displayNested,
+                                                           prefix));
+            }
+
+            return retVal;
+        }
+
+        /// <summary>Determins if a type is Enumerable.</summary>
+        private static bool IsTypeEnumerable(Type objectType)
+        {
+            foreach(Type i in objectType.GetInterfaces())
+            {
+                if(i == typeof(IEnumerable)
+                   || (i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+
+}
+
+
+#endif
---
+++ src/UI/Utility/MemberReference.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Reflection;
+
+using UnityEngine;
+
+namespace ModIO.UI
+{
+    /// <summary>Structure to use for displaying the member of an object.</summary>
+    [System.Serializable]
+    public struct MemberReference
+    {
+        // ---------[ NESTED DATA-TYPES ]---------
+        /// <summary>Property to use for the custom property drawer.</summary>
+        [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+        public class DropdownDisplayAttribute : PropertyAttribute
+        {
+            /// <summary>Type to use for reflection.</summary>
+            public Type objectType = null;
+            public bool displayEnumerables = false;
+            public bool displayNested = false;
+            public string[] membersToIgnore = null;
+
+            public DropdownDisplayAttribute(Type objectType,
+                                            bool displayEnumerables = false,
+                                            bool displayNested = false,
+                                            string[] membersToIgnore = null)
+            {
+                this.objectType = objectType;
+                this.displayEnumerables = displayEnumerables;
+                this.displayNested = displayNested;
+                this.membersToIgnore = membersToIgnore;
+            }
+        }
+
+        // ---------[ FIELDS ]---------
+        /// <summary>Field of the object to display.</summary>
+        [SerializeField]
+        private string m_memberPath;
+
+        /// <summary>Delegate sequence for fetching member value.</summary>
+        private Func<object, object>[] m_delegateSequence;
+
+        // ---------[ INITIALIZATION ]---------
+        /// <summary>Initialization</summary>
+        public MemberReference(string memberPath = null)
+        {
+            this.m_memberPath = memberPath;
+            this.m_delegateSequence = null;
+        }
+
+        /// <summary>Returns the value stored at the given member path.</summary>
+        public object GetValue(object objectInstance)
+        {
+            if(objectInstance == null) { return null; }
+
+            if(this.m_delegateSequence == null)
+            {
+                this.m_delegateSequence = MemberReference.BuildDelegateSequence(objectInstance.GetType(), this.m_memberPath);
+            }
+
+            if(this.m_delegateSequence.Length > 0)
+            {
+                object lastObject = objectInstance;
+                for(int i = 0; i < this.m_delegateSequence.Length && lastObject != null; ++i)
+                {
+                    lastObject = this.m_delegateSequence[i].Invoke(lastObject);
+                }
+
+                return lastObject;
+            }
+
+            return null;
+        }
+
+        // ---------[ UTILITY ]---------
+        /// <summary>Generates a sequence of delegates to fetch the value of the member at the given path.</summary>
+        private static Func<object, object>[] BuildDelegateSequence(Type objectType, string memberPath)
+        {
+            if(string.IsNullOrEmpty(memberPath)) { return new Func<object, object>[0]; }
+
+            string[] memberPathElements = memberPath.Split('.');
+            Func<object, object>[] delegates = new Func<object,object>[memberPathElements.Length];
+            Type lastObjectType = objectType;
+
+            for(int i = 0; i < delegates.Length && lastObjectType != null; ++i)
+            {
+                MemberInfo[] nextInfo = lastObjectType.GetMember(memberPathElements[i],
+                                                                 BindingFlags.Instance | BindingFlags.Public);
+                lastObjectType = null;
+
+                if(nextInfo.Length > 0 && nextInfo[0] != null)
+                {
+                    if(nextInfo[0] is FieldInfo)
+                    {
+                        FieldInfo fi = (FieldInfo)nextInfo[0];
+
+                        delegates[i] = fi.GetValue;
+                        lastObjectType = fi.FieldType;
+                    }
+                    else if(nextInfo[0] is PropertyInfo)
+                    {
+                        PropertyInfo pi = (PropertyInfo)nextInfo[0];
+
+                        delegates[i] = (o) => MemberReference.GetPropertyValue(pi, o);
+                        lastObjectType = pi.PropertyType;
+                    }
+                }
+            }
+
+            if(lastObjectType != null)
+            {
+                return delegates;
+            }
+
+            return new Func<object, object>[0];
+        }
+
+        /// <summary>Helper function for getting the value of a property.</summary>
+        private static object GetPropertyValue(PropertyInfo info, object objectInstance)
+        {
+            return info.GetValue(objectInstance, null);
+        }
+    }
+}
