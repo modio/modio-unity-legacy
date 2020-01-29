bdd97712f40239b12a7d55f8e3541dff911d570a
Jackson Wood <gamedev@melodatron.com>
2020-01-30 09:58:23 +1100

    Implemented Toggle.SetIsOnWithoutNotify (2019.1)

---
+++ src/_Compatibility/UIComponentExtensions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.UI;
+
+namespace ModIO.UI
+{
+    /// <summary>Provides compatibility fixes for functionality missing from earlier versions of Unity.</summary>
+    public static class UIComponentExtensions
+    {
+        #if !UNITY_2019_1_OR_NEWER
+
+        /// <summary>Allows a Toggle value to be set without triggering the onValueChanged event.</summary>
+        public static void SetIsOnWithoutNotify(this Toggle toggle, bool value)
+        {
+            Toggle.ToggleEvent oldEvent = toggle.onValueChanged;
+            toggle.onValueChanged = new Toggle.ToggleEvent();
+
+            toggle.isOn = value;
+
+            toggle.onValueChanged = oldEvent;
+        }
+
+        #endif
+    }
+}
