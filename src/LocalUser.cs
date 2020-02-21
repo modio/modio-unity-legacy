2590df2a6b1624e5ac66972463a9152f6df2873c
Jackson Wood <gamedev@melodatron.com>
2020-02-21 11:57:28 +1100

    Implemented singleton and Load functionality in LocalUser

--- src/LocalUser.cs
+++ src/LocalUser.cs
@@ -5,6 +5,13 @@ namespace ModIO
     /// <summary>Structure for storing data about a user specific to this device.</summary>
     public struct LocalUser
     {
+        // ---------[ Singleton ]---------
+        /// <summary>Singleton instance.</summary>
+        public static LocalUser instance;
+
+        /// <summary>Is the instance loaded?</summary>
+        public static bool isLoaded;
+
         // ---------[ FIELDS ]---------
         /// <summary>mod.io User Profile.</summary>
         public UserProfile profile;
@@ -45,6 +52,61 @@ namespace ModIO
                 else
                 {
                     return AuthenticationState.ValidToken;
+                }
+            }
+        }
+
+        // ---------[ Initialization ]---------
+        /// <summary>Sets the initial Singleton values.</summary>
+        static LocalUser()
+        {
+            LocalUser.isLoaded = false;
+            LocalUser.instance = new LocalUser();
+        }
+
+        /// <summary>Loads the LocalUser instance.</summary>
+        public static System.Collections.IEnumerator Load(System.Action callback)
+        {
+            bool isDone = false;
+
+            LocalUser.isLoaded = false;
+
+            UserDataStorage.TryReadJSONFile<LocalUser>(UserAccountManagement.USER_DATA_FILENAME, (success, fileData) =>
+            {
+                LocalUser.AssertListsNotNull(ref fileData);
+
+                LocalUser.instance = fileData;
+                LocalUser.isLoaded = success;
+
+                if(callback != null) { callback.Invoke(); }
+            });
+
+            while(!isDone) { yield return null; }
+        }
+
+        /// <summary>Asserts that the list fields are not null.</summary>
+        public static void AssertListsNotNull(ref LocalUser userData)
+        {
+            if(userData.enabledModIds == null
+               || userData.subscribedModIds == null
+               || userData.queuedSubscribes == null
+               || userData.queuedUnsubscribes == null)
+            {
+                if(userData.enabledModIds == null)
+                {
+                    userData.enabledModIds = new List<int>();
+                }
+                if(userData.subscribedModIds == null)
+                {
+                    userData.subscribedModIds = new List<int>();
+                }
+                if(userData.queuedSubscribes == null)
+                {
+                    userData.queuedSubscribes = new List<int>();
+                }
+                if(userData.queuedUnsubscribes == null)
+                {
+                    userData.queuedUnsubscribes = new List<int>();
                 }
             }
         }
