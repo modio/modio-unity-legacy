8c7582d235e6da6f57a2eb991aa55b1d9fd6b1b5
Jackson Wood <gamedev@melodatron.com>
2020-03-02 15:46:50 +1100

    Implemented pushing ModProfile to ModView

--- Runtime/UI/BrowserViews/ReportDialog.cs
+++ Runtime/UI/BrowserViews/ReportDialog.cs
@@ -10,6 +10,9 @@ namespace ModIO.UI
     public class ReportDialog : MonoBehaviour, IBrowserView, ICancelHandler
     {
         // ---------[ Fields ]---------
+        /// <summary>Mod id of the mod being reported.</summary>
+        private int m_modId = ModProfile.NULL_ID;
+
         // --- Accessors ---
         /// <summary>Gets the canvas group attached to this gameObject.</summary>
         CanvasGroup IBrowserView.canvasGroup { get { return this.gameObject.GetComponent<CanvasGroup>(); } }
@@ -21,6 +24,41 @@ namespace ModIO.UI
         bool IBrowserView.isRootView { get { return false; } }
 
         // ---------[ UI Control ]---------
+        /// <summary>Sets the mod id for the mod being reported.</summary>
+        public void SetModId(int modId)
+        {
+            if(this.m_modId != modId)
+            {
+                ModView view = this.GetComponent<ModView>();
+                if(view != null)
+                {
+                    view.profile = null;
+
+                    ModProfileRequestManager.instance.RequestModProfile(modId, this.SetModProfile, null);
+                }
+
+                this.m_modId = modId;
+            }
+        }
+
+        /// <summary>Sets the mod profile for the mod being reported.</summary>
+        public void SetModProfile(ModProfile profile)
+        {
+            ModView view = this.GetComponent<ModView>();
+            if(view != null)
+            {
+                view.profile = profile;
+            }
+
+            int newModId = ModProfile.NULL_ID;
+            if(profile != null)
+            {
+                newModId = profile.id;
+            }
+
+            this.m_modId = newModId;
+        }
+
         /// <summary>ICancelHandler interface to pass through to the cancel button.</summary>
         public void OnCancel(BaseEventData eventData)
         {
