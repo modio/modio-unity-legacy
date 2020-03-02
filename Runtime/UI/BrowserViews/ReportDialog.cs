478e3eaeaecc66ea2cfd8557b0c5d486bddfbd46
Jackson Wood <gamedev@melodatron.com>
2020-03-02 17:49:59 +1100

    Implemented url parameter filling and opening for the report widget

--- Runtime/UI/BrowserViews/ReportDialog.cs
+++ Runtime/UI/BrowserViews/ReportDialog.cs
@@ -9,9 +9,16 @@ namespace ModIO.UI
     /// <summary>A view for allowing a player to report a mod.</summary>
     public class ReportDialog : MonoBehaviour, IBrowserView, ICancelHandler
     {
+        // ---------[ Constants ]---------
+        /// <summary>URL for the mod.io report widget.</summary>
+        public static readonly string REPORT_WIDGET_URL = @"https://mod.io/report/widget";
+
         // ---------[ Fields ]---------
         /// <summary>Mod id of the mod being reported.</summary>
         private int m_modId = ModProfile.NULL_ID;
+
+        /// <summary>URL link for the report web widget.</summary>
+        private string m_reportURL = string.Empty;
 
         // --- Accessors ---
         /// <summary>Gets the canvas group attached to this gameObject.</summary>
@@ -29,34 +36,46 @@ namespace ModIO.UI
         {
             if(this.m_modId != modId)
             {
-                ModView view = this.GetComponent<ModView>();
-                if(view != null)
-                {
-                    view.profile = null;
-
-                    ModProfileRequestManager.instance.RequestModProfile(modId, this.SetModProfile, null);
-                }
-
                 this.m_modId = modId;
+
+                if(modId == ModProfile.NULL_ID)
+                {
+                    this.SetModProfile(null);
+                }
+                else
+                {
+                    ModProfileRequestManager.instance.RequestModProfile(modId, this.SetModProfile, (e) => this.m_modId = ModProfile.NULL_ID);
+                }
             }
         }
 
         /// <summary>Sets the mod profile for the mod being reported.</summary>
         public void SetModProfile(ModProfile profile)
         {
+            if(profile == null)
+            {
+                this.m_modId = ModProfile.NULL_ID;
+                this.m_reportURL = string.Empty;
+            }
+            else
+            {
+                this.m_modId = profile.id;
+
+                if(string.IsNullOrEmpty(profile.profileURL))
+                {
+                    this.m_reportURL = string.Empty;
+                }
+                else
+                {
+                    this.m_reportURL = "?urls=" + profile.profileURL;
+                }
+            }
+
             ModView view = this.GetComponent<ModView>();
             if(view != null)
             {
                 view.profile = profile;
             }
-
-            int newModId = ModProfile.NULL_ID;
-            if(profile != null)
-            {
-                newModId = profile.id;
-            }
-
-            this.m_modId = newModId;
         }
 
         /// <summary>ICancelHandler interface to pass through to the cancel button.</summary>
@@ -69,6 +88,12 @@ namespace ModIO.UI
         public void Close()
         {
             ViewManager.instance.CloseWindowedView(this);
+        }
+
+        /// <summary>Opens a web browser to the report form pre-filled with the mod URL.</summary>
+        public void OpenReportWidgetInWebBrowser()
+        {
+            Application.OpenURL(ReportDialog.REPORT_WIDGET_URL + this.m_reportURL);
         }
     }
 }
