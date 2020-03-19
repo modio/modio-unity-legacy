4f6fb0a0d976a406a7d0ac04fbbbf181561e7c35
Jackson Wood <gamedev@melodatron.com>
2020-03-19 17:51:02 +1100

    Removed SelectionFocusPriority component and associated rendering functionality

--- Runtime/UI/Utility/SelectionFocusPriority.cs
+++
@@ -1,9 +0,0 @@
-namespace ModIO.UI
-{
-    /// <summary>Defines the priority level of this selectable when a view receives focus.</summary>
-    public class SelectionFocusPriority : UnityEngine.MonoBehaviour
-    {
-        /// <summary>The priority level. Higher values are prioritized over lower values.</summary>
-        public int priority = 0;
-    }
-}
