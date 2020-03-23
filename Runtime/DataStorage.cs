671bb79e16a03b7d2bd13759eba42f36de82f3b8
Jackson Wood <gamedev@melodatron.com>
2020-03-24 10:53:34 +1100

    Created DataStorage to replace CacheClient

---
+++ Runtime/DataStorage.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Newtonsoft.Json;
+
+using ModIO.API;
+
+namespace ModIO
+{
+    /// <summary>An interface for storing/loading mod.io data on disk.</summary>
+    public static class DataStorage
+    {
+
+    }
+}
