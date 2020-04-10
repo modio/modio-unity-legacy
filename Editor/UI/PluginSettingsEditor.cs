2e6d86eba69c845224d0d519c126a958a6f84709
Jackson Wood <gamedev@melodatron.com>
2020-04-10 14:23:41 +1000

    Removed custom directory display code in PluginSettingsEditor

--- Editor/UI/PluginSettingsEditor.cs
+++ Editor/UI/PluginSettingsEditor.cs
@@ -26,11 +26,6 @@ namespace ModIO.UI.EditorCode
             {
                 SerializedProperty displayProp = propEnum.Current as SerializedProperty;
                 EditorGUILayout.PropertyField(displayProp);
-
-                if(displayProp.name.Contains("Directory"))
-                {
-                    DisplayProcessedDirectory(displayProp.stringValue);
-                }
             }
 
             EditorGUILayout.Space();
@@ -72,61 +67,6 @@ namespace ModIO.UI.EditorCode
                     Application.OpenURL(httpPrefix + @"mod.io/apikey");
                 }
             }
-        }
-
-        private void DisplayProcessedDirectory(string directoryValue)
-        {
-            string processedDir = PluginSettings.ReplaceDirectoryVariables(directoryValue,
-                                                                           gameIdProperty.intValue,
-                                                                           apiURLProperty.stringValue.StartsWith("https://api.test.mod.io"));
-
-            EditorGUILayout.BeginHorizontal();
-                using(new EditorGUI.DisabledScope(true))
-                {
-                    EditorGUILayout.LabelField(new GUIContent(processedDir, processedDir));
-                }
-
-                bool directoryIsValid = false;
-
-                try
-                {
-                    string testDir = processedDir;
-
-                    while(!directoryIsValid
-                          && !string.IsNullOrEmpty(testDir))
-                    {
-                        testDir = System.IO.Path.GetDirectoryName(testDir);
-                        directoryIsValid = System.IO.Directory.Exists(testDir);
-                    }
-                }
-                catch
-                {
-                    directoryIsValid = false;
-                }
-
-                using(new EditorGUI.DisabledScope(!directoryIsValid))
-                {
-                    string toolTip = null;
-                    if(directoryIsValid)
-                    {
-                        toolTip = "Locate directory";
-                    }
-                    else
-                    {
-                        toolTip = "Invalid directory";
-                    }
-
-                    if(GUILayout.Button(new GUIContent("...", toolTip), GUILayout.Width(21), GUILayout.Height(14)))
-                    {
-                        if(!System.IO.Directory.Exists(processedDir))
-                        {
-                            IOUtilities.CreateDirectory(processedDir);
-                        }
-
-                        EditorUtility.RevealInFinder(processedDir);
-                    }
-                }
-            EditorGUILayout.EndHorizontal();
         }
     }
 }
