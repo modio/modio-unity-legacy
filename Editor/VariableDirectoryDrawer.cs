31b2ff12cc15a4fcc7e5ed152d99b509d680dd5d
Jackson Wood <gamedev@melodatron.com>
2020-04-10 14:24:40 +1000

    Implemented VariableDirectoryDrawer

---
+++ Editor/VariableDirectoryDrawer.cs
@@ -0,0 +1,92 @@
+#if UNITY_EDITOR
+
+using System;
+
+using UnityEngine;
+using UnityEditor;
+
+namespace ModIO.UI.EditorCode
+{
+    [CustomPropertyDrawer(typeof(PluginSettings.VariableDirectoryAttribute))]
+    public class VariableDirectoryDrawer : PropertyDrawer
+    {
+        public const float BUTTON_WIDTH = 22;
+
+        // ---------[ GUI FUNCTIONALITY ]---------
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // render base field
+            Rect basePropertyRect = position;
+            basePropertyRect.height = position.height - EditorGUIUtility.singleLineHeight;
+            EditorGUI.PropertyField(basePropertyRect, property, label);
+
+            // draw unwrapped directory
+            string dir = property.stringValue;
+            dir = PluginSettings.ReplaceDirectoryVariables(dir,
+                                                           PluginSettings.data.gameId,
+                                                           PluginSettings.data.apiURL.Contains("api.test.mod.io"));
+
+            Rect previewRect = position;
+            previewRect.width = position.width - BUTTON_WIDTH;
+            previewRect.y = basePropertyRect.height + basePropertyRect.y;
+            previewRect.height = EditorGUIUtility.singleLineHeight;
+
+            Rect buttonRect = previewRect;
+            buttonRect.x = previewRect.xMax;
+            buttonRect.width = BUTTON_WIDTH;
+
+            using(new EditorGUI.DisabledScope(true))
+            {
+                // NOTE(@jackson): The second param of GUIContent is a tooltip
+                EditorGUI.LabelField(previewRect, new GUIContent(dir, dir));
+            }
+
+            bool directoryIsValid = false;
+            try
+            {
+                string testDir = dir;
+
+                while(!directoryIsValid && !string.IsNullOrEmpty(testDir))
+                {
+                    testDir = System.IO.Path.GetDirectoryName(testDir);
+                    directoryIsValid = System.IO.Directory.Exists(testDir);
+                }
+            }
+            catch
+            {
+                directoryIsValid = false;
+            }
+
+            using(new EditorGUI.DisabledScope(!directoryIsValid))
+            {
+                string toolTip = null;
+                if(directoryIsValid)
+                {
+                    toolTip = "Locate directory";
+                }
+                else
+                {
+                    toolTip = "Invalid directory";
+                }
+
+                if(GUI.Button(buttonRect, new GUIContent("...", toolTip)))
+                {
+                    if(!System.IO.Directory.Exists(dir))
+                    {
+                        IOUtilities.CreateDirectory(dir);
+                    }
+
+                    EditorUtility.RevealInFinder(dir);
+                }
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            float baseHeight = base.GetPropertyHeight(property, label);
+            return (baseHeight + EditorGUIUtility.singleLineHeight);
+        }
+    }
+}
+
+#endif // UNITY_EDITOR
