5124a2a958fd21d3e436c15c659862f3e1f0430d
Jackson Wood <gamedev@melodatron.com>
2020-04-17 16:35:03 +1000

    Minor documentation

--- Runtime/DataStorage/IPlatformIO.cs
+++ Runtime/DataStorage/IPlatformIO.cs
@@ -7,39 +7,39 @@ namespace ModIO
     public interface IPlatformIO
     {
         // --- File I/O ---
-        /// <summary>Delegate for reading a file.</summary>
+        /// <summary>Reads a file.</summary>
         byte[] ReadFile(string filePath);
 
-        /// <summary>Delegate for writing a file.</summary>
+        /// <summary>Writes a file.</summary>
         bool WriteFile(string filePath, byte[] data);
 
         // --- File Management ---
-        /// <summary>Delegate for deleting a file.</summary>
+        /// <summary>Deletes a file.</summary>
         bool DeleteFile(string filePath);
 
-        /// <summary>Delegate for moving a file.</summary>
+        /// <summary>Moves a file.</summary>
         bool MoveFile(string sourceFilePath, string destinationFilePath);
 
         /// <summary>Gets the size of a file.</summary>
         bool GetFileExists(string filePath);
 
-        /// <summary>Delegate for getting a file's size.</summary>
+        /// <summary>Gets the size of a file.</summary>
         Int64 GetFileSize(string filePath);
 
-        /// <summary>Delegate for getting a file's size and md5 hash.</summary>
+        /// <summary>Gets the size and md5 hash of a file.</summary>
         bool GetFileSizeAndHash(string filePath, out Int64 byteCount, out string md5Hash);
 
         // --- Directory Management ---
-        /// <summary>Delegate for creating a directory.</summary>
+        /// <summary>Creates a directory.</summary>
         bool CreateDirectory(string directoryPath);
 
-        /// <summary>Delegate for deleting a directory.</summary>
+        /// <summary>Deletes a directory.</summary>
         bool DeleteDirectory(string directoryPath);
 
         /// <summary>Moves a directory.</summary>
         bool MoveDirectory(string sourcePath, string destinationPath);
 
-        /// <summary>Delegate for getting the directories at a location.</summary>
+        /// <summary>Gets the sub-directories at a location.</summary>
         IList<string> GetDirectories(string directoryPath);
     }
 }
