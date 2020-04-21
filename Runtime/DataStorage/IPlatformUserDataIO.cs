1bb98fa2bc2debe18dc3c2ba1f66cf6201c30931
Jackson Wood <gamedev@melodatron.com>
2020-04-21 10:52:14 +1000

    IPlatformUserDataIO param rename

--- Runtime/DataStorage/IPlatformUserDataIO.cs
+++ Runtime/DataStorage/IPlatformUserDataIO.cs
@@ -7,38 +7,38 @@ namespace ModIO
     {
         // --- File I/O ---
         /// <summary>Reads a file.</summary>
-        void ReadFile(string filePath, ReadFileCallback callback);
+        void ReadFile(string path, ReadFileCallback callback);
 
         /// <summary>Writes a file.</summary>
-        void WriteFile(string filePath, byte[] data, WriteFileCallback callback);
+        void WriteFile(string path, byte[] data, WriteFileCallback callback);
 
         // --- File Management ---
         /// <summary>Deletes a file.</summary>
-        void DeleteFile(string filePath, DeleteFileCallback callback);
+        void DeleteFile(string path, DeleteFileCallback callback);
 
         /// <summary>Moves a file.</summary>
-        void MoveFile(string sourceFilePath, string destinationFilePath, MoveFileCallback callback);
+        void MoveFile(string source, string destination, MoveFileCallback callback);
 
         /// <summary>Checks for the existence of a file.</summary>
-        void GetFileExists(string filePath, GetFileExistsCallback callback);
+        void GetFileExists(string path, GetFileExistsCallback callback);
 
         /// <summary>Gets the size of a file.</summary>
-        void GetFileSize(string filePath, GetFileSizeCallback callback);
+        void GetFileSize(string path, GetFileSizeCallback callback);
 
         /// <summary>Gets the size and md5 hash of a file.</summary>
-        void GetFileSizeAndHash(string filePath, GetFileSizeAndHashCallback callback);
+        void GetFileSizeAndHash(string path, GetFileSizeAndHashCallback callback);
 
         // --- Directory Management ---
         /// <summary>Creates a directory.</summary>
-        void CreateDirectory(string directoryPath, CreateDirectoryCallback callback);
+        void CreateDirectory(string path, CreateDirectoryCallback callback);
 
         /// <summary>Deletes a directory.</summary>
-        void DeleteDirectory(string directoryPath, DeleteDirectoryCallback callback);
+        void DeleteDirectory(string path, DeleteDirectoryCallback callback);
 
         /// <summary>Moves a directory.</summary>
-        void MoveDirectory(string sourcePath, string destinationPath, MoveDirectoryCallback callback);
+        void MoveDirectory(string source, string destination, MoveDirectoryCallback callback);
 
         /// <summary>Gets the sub-directories at a location.</summary>
-        void GetDirectories(string directoryPath, GetDirectoriesCallback callback);
+        void GetDirectories(string path, GetDirectoriesCallback callback);
     }
 }
