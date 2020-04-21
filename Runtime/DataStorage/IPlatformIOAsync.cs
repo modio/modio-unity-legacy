234b3a650a0df5a63277c456f3255be874493acb
Jackson Wood <gamedev@melodatron.com>
2020-04-21 10:40:17 +1000

    Documentation

--- Runtime/DataStorage/IPlatformIOAsync.cs
+++ Runtime/DataStorage/IPlatformIOAsync.cs
@@ -6,39 +6,39 @@ namespace ModIO
     public interface IPlatformIOAsync
     {
         // --- File I/O ---
-        /// <summary>Delegate for reading a file.</summary>
+        /// <summary>Reads a file.</summary>
         void ReadFile(string filePath, ReadFileCallback callback);
 
-        /// <summary>Delegate for writing a file.</summary>
+        /// <summary>Writes a file.</summary>
         void WriteFile(string filePath, byte[] data, WriteFileCallback callback);
 
         // --- File Management ---
-        /// <summary>Delegate for deleting a file.</summary>
+        /// <summary>Deletes a file.</summary>
         void DeleteFile(string filePath, DeleteFileCallback callback);
 
-        /// <summary>Delegate for moving a file.</summary>
+        /// <summary>Moves a file.</summary>
         void MoveFile(string sourceFilePath, string destinationFilePath, MoveFileCallback callback);
 
-        /// <summary>Gets the size of a file.</summary>
+        /// <summary>Checks for the existence of a file.</summary>
         void GetFileExists(string filePath, GetFileExistsCallback callback);
 
-        /// <summary>Delegate for getting a file's size.</summary>
+        /// <summary>Gets the size of a file.</summary>
         void GetFileSize(string filePath, GetFileSizeCallback callback);
 
-        /// <summary>Delegate for getting a file's size and md5 hash.</summary>
+        /// <summary>Gets the size and md5 hash of a file.</summary>
         void GetFileSizeAndHash(string filePath, GetFileSizeAndHashCallback callback);
 
         // --- Directory Management ---
-        /// <summary>Delegate for creating a directory.</summary>
+        /// <summary>Creates a directory.</summary>
         void CreateDirectory(string directoryPath, CreateDirectoryCallback callback);
 
-        /// <summary>Delegate for deleting a directory.</summary>
+        /// <summary>Deletes a directory.</summary>
         void DeleteDirectory(string directoryPath, DeleteDirectoryCallback callback);
 
         /// <summary>Moves a directory.</summary>
         void MoveDirectory(string sourcePath, string destinationPath, MoveDirectoryCallback callback);
 
-        /// <summary>Delegate for getting the directories at a location.</summary>
+        /// <summary>Gets the sub-directories at a location.</summary>
         void GetDirectories(string directoryPath, GetDirectoriesCallback callback);
     }
 }
