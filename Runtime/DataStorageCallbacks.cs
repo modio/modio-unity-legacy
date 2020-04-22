8678ed106cb593cd217f743d8585cab1cfc9be6a
Jackson Wood <gamedev@melodatron.com>
2020-04-22 12:51:48 +1000

    Updated callback delegate definition to relative paths

--- Runtime/DataStorageCallbacks.cs
+++ Runtime/DataStorageCallbacks.cs
@@ -9,26 +9,26 @@ namespace ModIO.UserDataIOCallbacks
 
     // --- File I/O ---
     /// <summary>Delegate for ReadFile callback.</summary>
-    public delegate void ReadFileCallback(string path, bool success, byte[] data);
+    public delegate void ReadFileCallback(string relativePath, bool success, byte[] data);
 
     /// <summary>Delegate for ReadJSONFile callback.</summary>
-    public delegate void ReadJSONFileCallback<T>(string path, bool success, T jsonObject);
+    public delegate void ReadJSONFileCallback<T>(string relativePath, bool success, T jsonObject);
 
     /// <summary>Delegate for WriteFile callbacks.</summary>
-    public delegate void WriteFileCallback(string path, bool success);
+    public delegate void WriteFileCallback(string relativePath, bool success);
 
     // --- File Management ---
     /// <summary>Delegate for DeleteFile callbacks.</summary>
-    public delegate void DeleteFileCallback(string path, bool success);
+    public delegate void DeleteFileCallback(string relativePath, bool success);
 
     /// <summary>Delegate for GetFileExists callback.</summary>
-    public delegate void GetFileExistsCallback(string path, bool doesExist);
+    public delegate void GetFileExistsCallback(string relativePath, bool doesExist);
 
     /// <summary>Delegate for GetFileSize callback.</summary>
-    public delegate void GetFileSizeCallback(string path, Int64 byteCount);
+    public delegate void GetFileSizeCallback(string relativePath, Int64 byteCount);
 
     /// <summary>Delegate for GetFileSizeAndHash callback.</summary>
-    public delegate void GetFileSizeAndHashCallback(string path, bool success, Int64 byteCount, string md5Hash);
+    public delegate void GetFileSizeAndHashCallback(string relativePath, bool success, Int64 byteCount, string md5Hash);
 
     /// <summary>Delegate for ClearActiveUserData callback.</summary>
     public delegate void ClearActiveUserDataCallback(bool success);
