9c62970eb4b6e23f140aa7e9ce74e34d7315512f
Jackson Wood <jackson@mod.io>
2020-07-13 17:34:12 +1000

    Created async IPlatformIO interface

--- Runtime/DataStorage/IPlatformIO.cs
+++ Runtime/DataStorage/IPlatformIO.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 
+using ModIO.LocalDataIOCallbacks;
+
 namespace ModIO
 {
     /// <summary>Defines the functions necessary for a complete platform IO.</summary>
@@ -47,5 +49,48 @@ namespace ModIO
 
         /// <summary>Gets the sub-directories at a location.</summary>
         IList<string> GetDirectories(string path);
+    }
+
+    /// <summary>Defines the functions necessary for a complete platform IO.</summary>
+    public interface IPlatformIO_Async
+    {
+        // --- File I/O ---
+        /// <summary>Reads a file.</summary>
+        void ReadFile(string path, out byte[] data, ReadFileCallback callback);
+
+        /// <summary>Writes a file.</summary>
+        void WriteFile(string path, byte[] data, WriteFileCallback callback);
+
+        // --- File Management ---
+        /// <summary>Deletes a file.</summary>
+        void DeleteFile(string path, DeleteFileCallback callback);
+
+        /// <summary>Moves a file.</summary>
+        void MoveFile(string source, string destination, MoveFileCallback callback);
+
+        /// <summary>Checks for the existence of a file.</summary>
+        void GetFileExists(string path, GetFileExistsCallback callback);
+
+        /// <summary>Gets the size and md5 hash of a file.</summary>
+        void GetFileSizeAndHash(string path, out Int64 byteCount, out string md5Hash, GetFileSizeAndHashCallback callback);
+
+        /// <summary>Gets the files at a location.</summary>
+        void GetFiles(string path, string nameFilter, bool recurseSubdirectories, GetFilesCallback callback);
+
+        // --- Directory Management ---
+        /// <summary>Creates a directory.</summary>
+        void CreateDirectory(string path, CreateDirectoryCallback callback);
+
+        /// <summary>Deletes a directory.</summary>
+        void DeleteDirectory(string path, DeleteDirectoryCallback callback);
+
+        /// <summary>Moves a directory.</summary>
+        void MoveDirectory(string source, string destination, MoveDirectoryCallback callback);
+
+        /// <summary>Checks for the existence of a directory.</summary>
+        void GetDirectoryExists(string path, GetDirectoryExistsCallback callback);
+
+        /// <summary>Gets the sub-directories at a location.</summary>
+        void GetDirectories(string path, GetDirectoriesCallback callback);
     }
 }
