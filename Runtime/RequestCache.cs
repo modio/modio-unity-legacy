0e5b328f20076c22cc8cd5df82db76e863c05e8e
Jackson Wood <jackson@mod.io>
2021-01-14 13:32:08 +1100

    Added timestamps to the RequestCache.Entry

--- Runtime/RequestCache.cs
+++ Runtime/RequestCache.cs
@@ -5,17 +5,35 @@ namespace ModIO
     /// <summary>Holds a collection of responses for URLs.</summary>
     public static class RequestCache
     {
+        // ---------[ Nested Data-Types ]---------
+        /// <summary>Entry for a stored response in the request cache.</summary>
+        public struct Entry
+        {
+            public int timeStamp;
+            public string responseBody;
+        }
+
         // ---------[ Fields ]---------
         /// <summary>Map of url to saved responses.</summary>
-        public static Dictionary<string, string> storedResponses
-            = new Dictionary<string, string>();
+        public static Dictionary<string, Entry> storedResponses
+            = new Dictionary<string, Entry>();
 
         /// <summary>Fetches a response from the cache.</summary>
         public static bool TryGetResponse(string url, out string response)
         {
             bool success = false;
 
-            success = RequestCache.storedResponses.TryGetValue(url, out response);
+            Entry entry;
+            success = RequestCache.storedResponses.TryGetValue(url, out entry);
+
+            if(success)
+            {
+                response = entry.responseBody;
+            }
+            else
+            {
+                response = null;
+            }
 
             return success;
         }
@@ -23,7 +41,13 @@ namespace ModIO
         /// <summary>Stores a response in the cache.</summary>
         public static void StoreResponse(string url, string responseBody)
         {
-            RequestCache.storedResponses[url] = responseBody;
+            Entry entry = new Entry()
+            {
+                timeStamp = ServerTimeStamp.Now,
+                responseBody = responseBody,
+            };
+
+            RequestCache.storedResponses[url] = entry;
         }
     }
 }
