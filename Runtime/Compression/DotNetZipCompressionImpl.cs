e23cddab92f4b08a11bc67b04b05ea8ede0255cf
Jackson Wood <jackson@mod.io>
2021-05-12 12:05:38 +1000

    Implemented DNZCompression.CompressFileList

--- Runtime/Compression/DotNetZipCompressionImpl.cs
+++ Runtime/Compression/DotNetZipCompressionImpl.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 
 using Exception = System.Exception;
 using Debug = UnityEngine.Debug;
+using Path = System.IO.Path;
 
 namespace ModIO
 {
@@ -38,7 +39,38 @@ namespace ModIO
         /// <summary>Compresses the contents of a file collection into an output archive.</summary>
         public bool CompressFileCollection(string rootDirectory, IEnumerable<string> fileCollection, string outputPath)
         {
-            throw new System.NotImplementedException();
+            bool success = false;
+            string lastFilePath = string.Empty;
+
+            try
+            {
+                using(var zip = new Ionic.Zip.ZipFile())
+                {
+                    foreach(string filePath in fileCollection)
+                    {
+                        lastFilePath = filePath;
+
+                        string relativeFilePath = filePath.Substring(rootDirectory.Length);
+                        string relativeDirectory = Path.GetDirectoryName(relativeFilePath);
+
+                        zip.AddFile(filePath, relativeDirectory);
+                    }
+
+                    zip.Save(outputPath);
+
+                    success = true;
+                }
+            }
+            catch(Exception e)
+            {
+                Debug.LogWarning("[mod.io] Unable to compress file collection to archive."
+                                 + "\nLast Attempted File: " + lastFilePath
+                                 + "\nOutput: " + outputPath
+                                 + "\n\n"
+                                 + Utility.GenerateExceptionDebugString(e));
+            }
+
+            return success;
         }
 
         /// <summary>Compresses a single file into an output archive.</summary>
