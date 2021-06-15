9f01799ddbcf353232bc17e8735d388a8a33a71b
Jackson Wood <jackson@mod.io>
2021-06-15 15:51:08 +1000

    Copied UpdateVersionedValues into VersionedDataAttribute

--- Runtime/Utility/VersionedDataAttribute.cs
+++ Runtime/Utility/VersionedDataAttribute.cs
@@ -3,13 +3,51 @@
     /// <summary>Attribute for determining the version of and default value for a given field.</summary>
     public class VersionedDataAttribute : System.Attribute
     {
+        // ---------[ Fields ]---------
+        /// <summary>Version the field was created.</summary>
         public int version;
+
+        /// <summary>Default value for the field.</summary>
         public System.Object defaultValue;
 
+        // ---------[ Initialization ]---------
         public VersionedDataAttribute(int version, System.Object defaultValue)
         {
             this.version = version;
             this.defaultValue = defaultValue;
+        }
+
+        // ---------[ Utility ]---------
+        /// <summary>Creates an updated version of passed PluginSettings.Data.</summary>
+        public static T UpdateVersionedValues<T>(int dataVersion, T dataValues)
+        where T : struct
+        {
+            // set up data
+            T updatedValues = dataValues;
+            System.Object boxedData = updatedValues;
+
+            // iterate over the typed fields
+            var fieldList = typeof(T).GetFields();
+            foreach(var field in fieldList)
+            {
+                // check for the VersionedDataAttribute attribute
+                var attributeList = field.GetCustomAttributes(typeof(VersionedDataAttribute), false);
+                if(attributeList != null
+                   && attributeList.Length == 1)
+                {
+                    // set the default value if attribute is newer than the dataVersion
+                    VersionedDataAttribute dataAttribute = (VersionedDataAttribute)attributeList[0];
+                    if(dataAttribute.version > dataVersion)
+                    {
+                        field.SetValue(boxedData, dataAttribute.defaultValue);
+                    }
+                }
+            }
+
+            //  unbox the updatedValues
+            updatedValues = (T)boxedData;
+
+            return updatedValues;
         }
     }
 }
