983abcd99790cacf5add9fdbe7c8de0531944074
Jackson Wood <jackson@mod.io>
2021-07-07 17:34:07 +1000

    Removed obsoletes from ModTeamMemberAccessLevel

--- Runtime/Enums/ModTeamMemberAccessLevel.cs
+++ Runtime/Enums/ModTeamMemberAccessLevel.cs
@@ -5,8 +5,5 @@ namespace ModIO
         Moderator = 1, // can moderate comments and content attached
         Manager = 4, // moderator access, including uploading builds and editing settings except supply and team members
         Administrator = 8, // full access, including editing the supply and team
-
-        [System.Obsolete("Replaced by ModTeamMemberAccessLevel.Manager")]
-        Statistics = 4,
     }
 }
