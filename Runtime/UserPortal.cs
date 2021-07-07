93826ceb2fbcf457a25306ac92f231eb3ff0fb32
Jackson Wood <jackson@mod.io>
2021-07-07 14:44:07 +1000

    Added UserPortal enum

---
+++ Runtime/UserPortal.cs
@@ -0,0 +1,17 @@
+namespace ModIO
+{
+    /// <summary>Values representing the valid User Portals that mod.io works with.</summary>
+    public enum UserPortal
+    {
+        None = 0,
+        Apple,
+        EpicGamesStore,
+        GOG,
+        Google,
+        itchio,
+        Nintendo,
+        PlaystationNetwork,
+        Steam,
+        XboxLive,
+    }
+}
