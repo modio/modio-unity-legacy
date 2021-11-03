af969dfc481832f9393e4fae2b3e6af2599a945c
Jackson Wood <jackson@mod.io>
2021-11-03 12:44:03 +1100

    Added UnityWebRequestExtensions with Unity 2020+ code path

---
+++ Runtime/_Compatibility/UnityWebRequestExtensions.cs
@@ -0,0 +1,20 @@
+using UnityEngine.Networking;
+
+namespace ModIO
+{
+    /// <summary>Adds extensions to the UnityWebRequest class.</summary>
+    public static class UnityWebRequestExtensions
+    {
+        /// <summary>Wraps the various error-types for earlier versions of Unity.</summary>
+        public static bool IsError(this UnityWebRequest webRequest)
+        {
+#if UNITY_2020_1_OR_NEWER
+
+            return (webRequest.result == UnityWebRequest.Result.ConnectionError
+                    && webRequest.result == UnityWebRequest.Result.ProtocolError
+                    && webRequest.result == UnityWebRequest.Result.DataProcessingError);
+
+#endif // Unity Version Selector
+        }
+    }
+}
